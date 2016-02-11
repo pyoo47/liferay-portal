9214e8781bb5259ae3c59df38ac3265c72935363
Kevin Yen <kevin.yen@liferay.com>
2016-02-11 11:49:18 -0800

    LRQA-21395 Avoid matching starting tag since starting tag can be "beanshell" or "echo"

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/GitHubMessageUtilTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/GitHubMessageUtilTest.java
@@ -241,6 +241,6 @@ public class GitHubMessageUtilTest extends BaseJenkinsResultsParserTestCase {
 		".+://(?<hostName>[^.]+).liferay.com/job/(?<jobName>[^/]+).*/" +
 			"(?<buildNumber>\\d+)/");
 	private static final Pattern _progressiveTextPattern = Pattern.compile(
-		"\\[echo\\] \\'.*\\' completed at (?<url>.+)\\.");
+		"\\'.*\\' completed at (?<url>.+)\\.");
 
 }
