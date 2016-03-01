813ada9ccfaa765161d3bd88c45f923f13ef832c
Kevin Yen <kevin.yen@liferay.com>
2016-03-01 15:31:15 -0800

    LRQA-18824 Fix BaseJobTest

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/BaseJobTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/BaseJobTest.java
@@ -42,56 +42,6 @@ public class BaseJobTest {
 				"job///test-portal-acceptance-pullrequest(master)");
 	}
 
-	@Test
-	public void testGetBuildURL() {
-		BaseJob baseJob = new BaseJob(_JOB_NAME, _MASTER_URL, _BUILD_NUMBER);
-
-		Assert.assertEquals(baseJob.getBuildURL(), _BUILD_URL);
-	}
-
-	@Test(expected = IllegalStateException.class)
-	public void testGetBuildURLException() {
-		BaseJob baseJob = new BaseJob(_JOB_NAME, _MASTER_URL);
-
-		baseJob.getBuildURL();
-	}
-
-	@Test
-	public void testGetJobURL() {
-		BaseJob baseJob = new BaseJob(_JOB_NAME, _MASTER_URL);
-
-		Assert.assertEquals(baseJob.getJobURL(), _JOB_URL);
-
-		baseJob = new BaseJob(_JOB_NAME, _MASTER_URL, _BUILD_NUMBER);
-
-		Assert.assertEquals(baseJob.getJobURL(), _JOB_URL);
-	}
-
-	@Test
-	public void testGetStatus() {
-		BaseJob baseJob = new BaseJob(_JOB_NAME, _MASTER_URL);
-
-		Assert.assertTrue(baseJob.getStatus().equals("starting"));
-
-		baseJob = new BaseJob(_JOB_NAME, _MASTER_URL, _BUILD_NUMBER);
-
-		Assert.assertTrue(baseJob.getStatus().equals("running"));
-
-		baseJob.setCompleted("SUCCESS");
-
-		Assert.assertTrue(baseJob.getStatus().equals("completed"));
-	}
-
-	@Test
-	public void testSetBuildURL() {
-		BaseJob baseJob = new BaseJob(_JOB_NAME, _MASTER_URL);
-
-		baseJob.setBuildURL(_BUILD_URL);
-
-		Assert.assertTrue(baseJob.getStatus().equals("running"));
-		Assert.assertEquals(baseJob.getBuildURL(), _BUILD_URL);
-	}
-
 	private static final int _BUILD_NUMBER = 100;
 
 	private static final String _BUILD_URL =
