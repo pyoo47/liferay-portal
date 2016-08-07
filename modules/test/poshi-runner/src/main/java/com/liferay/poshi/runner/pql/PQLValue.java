099f04cff4965c4d49eb5f980b1bcad164002d37
Michael Hashimoto <michael.hashimoto@liferay.com>
2016-08-07 16:13:22 -0700

    LRQA-26691 Add 'PQLEntity' implementation called 'PQLValue'

---
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLValue.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.pql;
+
+import java.util.Properties;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class PQLValue extends PQLEntity {
+
+	public PQLValue(String query) throws Exception {
+		super(query);
+
+		_validateValue(query);
+	}
+
+	public Object getValue(Properties properties) throws Exception {
+		return getObjectValue(getFixedQuery());
+	}
+
+	protected Object getObjectValue(String value) throws Exception {
+		_validateValue(value);
+
+		if (value == null) {
+			return null;
+		}
+
+		if ((value.startsWith("'") && value.endsWith("'")) ||
+			(value.startsWith("\"") && value.endsWith("\""))) {
+
+			value = value.substring(1, value.length() - 1);
+		}
+		else if (value.contains(" ")) {
+			throw new Exception("Invalid query: " + value);
+		}
+
+		Object objectValue;
+
+		if (value.equals("null")) {
+			return null;
+		}
+		else if (value.equals("true") || value.equals("false")) {
+			objectValue = Boolean.valueOf(value);
+		}
+		else if (value.matches("\\d+\\.\\d+")) {
+			objectValue = Double.valueOf(value);
+		}
+		else if (value.matches("\\d+")) {
+			objectValue = Integer.valueOf(value);
+		}
+		else {
+			objectValue = value;
+		}
+
+		PQLModifier pqlModifier = getPQLModifier();
+
+		if (pqlModifier != null) {
+			objectValue = pqlModifier.modify(objectValue);
+		}
+
+		return objectValue;
+	}
+
+	private static void _validateValue(String value) throws Exception {
+		if (value == null) {
+			return;
+		}
+
+		value.trim();
+
+		if ((value.startsWith("'") && value.endsWith("'")) ||
+			(value.startsWith("\"") && value.endsWith("\""))) {
+
+			return;
+		}
+
+		if (value.contains(" ")) {
+			throw new Exception("Invalid query: " + value);
+		}
+	}
+
+}
