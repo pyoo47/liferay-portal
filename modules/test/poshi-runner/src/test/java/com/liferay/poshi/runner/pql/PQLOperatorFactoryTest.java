b79bf53ae08044f9c2114b274df4a5873246b1c4
Michael Hashimoto <michael.hashimoto@liferay.com>
2016-08-07 18:58:24 -0700

    LRQA-26691 Add unit tests for 'PQLOperatorFactory'

---
+++ modules/test/poshi-runner/src/test/java/com/liferay/poshi/runner/pql/PQLOperatorFactoryTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.pql;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class PQLOperatorFactoryTest extends TestCase {
+
+	@Test
+	public void testNewInstance() throws Exception {
+		Set<String> availableOperators = PQLOperator.getAvailableOperators();
+
+		for (String operator : availableOperators) {
+			PQLOperator pqlOperator = PQLOperatorFactory.newInstance(operator);
+		}
+	}
+
+	@Test
+	public void testNewInstanceError() throws Exception {
+		Set<String> operators = new HashSet<>();
+
+		operators.add(null);
+		operators.add("bad");
+		operators.add("bad value");
+		operators.addAll(PQLModifier.getAvailableModifiers());
+
+		for (String operator : operators) {
+			_validateNewInstanceError(
+				operator, "Invalid '" + operator + "' operator.");
+		}
+	}
+
+	private void _validateNewInstanceError(String operator, String expected)
+		throws Exception {
+
+		String actual = null;
+
+		try {
+			PQLOperator pqlOperator = PQLOperatorFactory.newInstance(operator);
+		}
+		catch (Exception e) {
+			actual = e.getMessage();
+
+			if (!actual.equals(expected)) {
+				StringBuilder sb = new StringBuilder();
+
+				sb.append("Mismatched throw error:\n");
+				sb.append("\n\n* Actual:   \"");
+				sb.append(actual);
+				sb.append("\"\n* Expected: \"");
+				sb.append(expected);
+				sb.append("\"");
+
+				throw new Exception(sb.toString(), e);
+			}
+		}
+		finally {
+			if (actual == null) {
+				throw new Exception(
+					"No error thrown for the following operator: " + operator);
+			}
+		}
+	}
+
+}
