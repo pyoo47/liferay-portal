1078c6e223fbe9039bcd8619231a5e084a8e1352
Peter Yoo <peter.yoo@liferay.com>
2016-12-28 09:00:21 -0800

    LRQA-29640 Make archiveBuildURLPattern protected.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AxisBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AxisBuild.java
@@ -288,7 +288,7 @@ public class AxisBuild extends BaseBuild {
 		Matcher matcher = buildURLPattern.matcher(buildURL);
 
 		if (!matcher.find()) {
-			matcher = _archiveBuildURLPattern.matcher(buildURL);
+			matcher = archiveBuildURLPattern.matcher(buildURL);
 
 			if (!matcher.find()) {
 				throw new IllegalArgumentException(
@@ -309,6 +309,10 @@ public class AxisBuild extends BaseBuild {
 		setStatus("running");
 	}
 
+	protected static final Pattern archiveBuildURLPattern = Pattern.compile(
+		"(\\$\\{dependencies\\.url\\}|file:|http://).*/(?<archiveName>[^/]+)/" +
+			"(?<master>[^/]+)/+(?<jobName>[^/]+)/(?<axisVariable>" +
+				"AXIS_VARIABLE=[^,]+,[^/]+)/(?<buildNumber>\\d+)/?");
 	protected static final Pattern buildURLPattern = Pattern.compile(
 		"\\w+://(?<master>[^/]+)/+job/+(?<jobName>[^/]+)/" +
 			"(?<axisVariable>AXIS_VARIABLE=[^,]+,[^/]+)/" +
@@ -318,10 +322,6 @@ public class AxisBuild extends BaseBuild {
 
 	protected String axisVariable;
 
-	private static final Pattern _archiveBuildURLPattern = Pattern.compile(
-		"($\\{dependencies\\.url\\}|file:|http://).*/(?<archiveName>[^/]+)/" +
-			"(?<master>[^/]+)/+(?<jobName>[^/]+)/(?<axisVariable>" +
-				"AXIS_VARIABLE=[^,]+,[^/]+)/(?<buildNumber>\\d+)/?");
 	private static final Pattern _axisVariablePattern = Pattern.compile(
 		"AXIS_VARIABLE=(?<axisNumber>[^,]+),.*");
 
