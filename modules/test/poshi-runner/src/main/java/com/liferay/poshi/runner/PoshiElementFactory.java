2421beb92ab43de93fe140c79f8a2775eebecb79
Kenji Heigel <kenji.heigel@liferay.com>
2017-06-01 11:10:55 -0700

    LRQA-32801 Update poshi element factory to accept readable syntax and testcase file paths

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiElementFactory.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiElementFactory.java
@@ -14,6 +14,24 @@
 
 package com.liferay.poshi.runner;
 
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.AND;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.FEATURE;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.GIVEN;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.SCENARIO;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.SETUP;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.TEARDOWN;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.THEN;
+import static com.liferay.poshi.runner.ReadableSyntaxKeys.WHEN;
+import static com.liferay.poshi.runner.util.StringPool.PIPE;
+
+import com.liferay.poshi.runner.util.Dom4JUtil;
+import com.liferay.poshi.runner.util.FileUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.StringReader;
+
+import org.dom4j.Document;
 import org.dom4j.Element;
 
 /**
@@ -55,6 +73,82 @@ public class PoshiElementFactory {
 		}
 
 		return new UnsupportedElement(element, parentElement);
+	}
+
+	public static PoshiElement newPoshiElement(String filePath) {
+		File file = new File(filePath);
+
+		try {
+			String fileContent = FileUtil.read(file);
+
+			if (fileContent.contains("<definition")) {
+				Document document = Dom4JUtil.parse(fileContent);
+
+				Element rootElement = document.getRootElement();
+
+				return newPoshiElement(rootElement, null);
+			}
+
+			return newPoshiElement(fileContent, null);
+		}
+		catch (Exception e) {
+			System.out.println("The Poshi element could not be generated.");
+
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static PoshiElement newPoshiElement(
+		String readableSyntax, PoshiElement parentElement) {
+
+		try (BufferedReader bufferedReader = new BufferedReader(
+				new StringReader(readableSyntax))) {
+
+			String line = null;
+
+			while ((line = bufferedReader.readLine()) != null) {
+				line = line.trim();
+
+				if (line.length() == 0) {
+					continue;
+				}
+
+				if (line.startsWith(FEATURE)) {
+					return new TestFileElement(readableSyntax, parentElement);
+				}
+
+				if (line.startsWith(SCENARIO)) {
+					return new CommandElement(readableSyntax, parentElement);
+				}
+
+				if (line.startsWith(SETUP)) {
+					return new SetUpElement(readableSyntax, parentElement);
+				}
+
+				if (line.startsWith(TEARDOWN)) {
+					return new TearDownElement(readableSyntax, parentElement);
+				}
+
+				if (line.startsWith(AND) || line.startsWith(GIVEN) ||
+					line.startsWith(THEN) || line.startsWith(WHEN)) {
+
+					return new ExecuteElement(readableSyntax, parentElement);
+				}
+
+				if (line.startsWith(PIPE)) {
+					return new VarElement(readableSyntax, parentElement);
+				}
+			}
+		}
+		catch (Exception e) {
+			System.out.println("The Poshi element could not be generated.");
+
+			e.printStackTrace();
+		}
+
+		return new UnsupportedElement(readableSyntax, parentElement);
 	}
 
 }
