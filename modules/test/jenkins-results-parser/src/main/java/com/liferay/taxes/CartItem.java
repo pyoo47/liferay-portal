026eb388ac9f2d6ecbe9e57432499af1fabf23b0
cesar-polanco <cesar.polanco@liferay.com>
2017-06-20 15:57:46 -0700

    Fixing Merge conflicts

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/taxes/CartItem.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/taxes/CartItem.java
@@ -13,211 +13,285 @@ import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.*;
 import java.nio.file.StandardOpenOption;
 
- public class CartItem {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    static String FOODFILE = "food_file.txt";
-    static String MEDICINEFILE = "medicine_file.txt";
-    static String SEENFILE = "all_seen_products.txt";
-    StandardOpenOption[] options = new StandardOpenOption[] { APPEND, CREATE};
-    boolean basicSalesTaxApplicable;
-    boolean importSalesTaxApplicable;
-    String name;
-    BigDecimal preTaxPrice;
-    BigDecimal postTaxPrice;
-    int quantity;
+/**
+ * @author Cesar Polanco
+ */
+public class CartItem {
 
-    public CartItem(int amount, String productName, String extPrice){
-      quantity = amount;
-      name = productName;
-      basicSalesTaxApplicable = checkBasicTaxability(productName);
-      importSalesTaxApplicable = checkImportTaxability(productName);
-      preTaxPrice = new BigDecimal(extPrice);
-    }
+	public CartItem(int amount, String productName, String extPrice) {
+		_quantity = amount;
+		_name = productName;
+		_basicSalesTaxApplicable = checkBasicTaxability(productName);
+		_importSalesTaxApplicable = _checkImportTaxability(productName);
+		_preTaxPrice = new BigDecimal(extPrice);
+	}
 
-    public BigDecimal getPreTaxPrice(){
-      return preTaxPrice;
-    }
+	public boolean checkBasicTaxability(String productName) {
+		if (_checkIfSeen(productName)) {
+			if (!(_checkIfFood(productName) ||
+				 _checkIfMedicine(productName) ||
+				_checkIfBook(productName))) {
 
-    public void setPostTaxPrice(BigDecimal taxTotal){
-      postTaxPrice = preTaxPrice.add(taxTotal);
-    }
+				return true;
+			}
 
-    public BigDecimal getPostTaxPrice(){
-      return postTaxPrice;
-    }
+			return false;
+		}
+		else {
+			_addToSeen(productName);
 
-    public boolean isTaxable(){
-      return basicSalesTaxApplicable;
-    }
+			if (_checkIfBook(productName)) {
+				return false;
+			}
+			else if (_askIfFood(productName)) {
+				_addToFood(productName);
+				return false;
+			}
+			else if (_askIfMedicine(productName)) {
+				_addToMedicine(productName);
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
 
-    public boolean isImported(){
-      return importSalesTaxApplicable;
-    }
+	public String getName() {
+		return _name;
+	}
 
-    public String getName(){
-      return name;
-    }
+	public BigDecimal getPostTaxPrice() {
+		return _postTaxPrice;
+	}
 
-    public int getQuantity(){
-      return quantity;
-    }
+	public BigDecimal getPreTaxPrice() {
+		return _preTaxPrice;
+	}
 
-    private boolean checkImportTaxability(String productName){
-      if(Arrays.asList(productName.split(" ")).contains("imported")){
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
+	public int getQuantity() {
+		return _quantity;
+	}
 
-    public boolean checkBasicTaxability(String productName){
-      if(checkIfSeen(productName)){
-        return (!(checkIfFood(productName) || checkIfMedicine(productName) || checkIfBook(productName)));
-      }
-      else {
-        addToSeen(productName);
-        if(checkIfBook(productName)){
-          return false;
-        }
-        else if(askIfFood(productName)){
-          addToFood(productName);
-          return false;
-        }
-        else if(askIfMedicine(productName)){
-          addToMedicine(productName);
-          return false;
-        }
-        else{
-          return true;
-        }
-      }
-    }
+	public boolean isImported() {
+		return _importSalesTaxApplicable;
+	}
 
-    private boolean checkIfBook(String productName){
-      return (productName.equals("book") || productName.equals("books"));
-    }
+	public boolean isTaxable() {
+		return _basicSalesTaxApplicable;
+	}
 
-    private boolean checkIfSeen(String productName){
-      // Consult SEEN file
-      boolean existsInSeen = false;
-      try{
-        existsInSeen = Files.lines(Paths.get(SEENFILE)).anyMatch((item) -> item.equals(productName));
-      } catch (IOException e){
-        //e.printStackTrace();
-        try{
-          Files.createFile(Paths.get(SEENFILE));
-          checkIfSeen(productName);
-        } catch (IOException x){
-          x.printStackTrace();
-        }
-      }
-      return existsInSeen;
-    }
+	public void setPostTaxPrice(BigDecimal taxTotal) {
+		_postTaxPrice = _preTaxPrice.add(taxTotal);
+	}
 
-    private boolean checkIfFood(String productName){
-      // consult Food file
-      boolean existsInFood = false;
-      try{
-        existsInFood = Files.lines(Paths.get(FOODFILE)).anyMatch((item) -> item.equals(productName));
-      } catch (IOException e){
-        //e.printStackTrace();
-        try{
-          Files.createFile(Paths.get(FOODFILE));
-          checkIfFood(productName);
-        } catch (IOException x){
-          x.printStackTrace();
-        }
-      }
-      return existsInFood;
-    }
+	private void _addToFood(String productName) {
 
-    private boolean checkIfMedicine(String productName){
-      // Consult MEDICINE file
-      boolean existsInMedicine = false;
-      try{
-        existsInMedicine = Files.lines(Paths.get(MEDICINEFILE)).anyMatch((item) -> item.equals(productName));
-      } catch (IOException e){
-        //e.printStackTrace();
-        try{
-          Files.createFile(Paths.get(MEDICINEFILE));
-          checkIfMedicine(productName);
-        } catch (IOException x){
-          x.printStackTrace();
-        }
-      }
-      return existsInMedicine;
-    }
+		// Add to Food file
 
-    private boolean askIfFood(String productName){
-      //Ask via commandline if this product is Food
-      System.out.println("This product has not been seen before. Please answer the following for tax purposes:");
-      boolean answer = false;
-      Console console = null;
-      String response = null;
-      try {
-        console = System.console();
-        if(console != null){
-          response = console.readLine("Is " + productName + " food: ");
-          if(response.equals("yes") || response.equals("Yes")){
-            answer = true;
-          }
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      return answer;
-    }
+		ArrayList<String> iterableProduct = new ArrayList<>();
 
-    private boolean askIfMedicine(String productName){
-      // Ask via commandline if this product is Medicine
-      boolean answer = false;
-      Console console = null;
-      String response = null;
-      try {
-        console = System.console();
-        if(console != null){
-          response = console.readLine("Is " + productName + " medicine: ");
-          if(response.equals("yes") || response.equals("Yes")){
-            answer = true;
-          }
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      return answer;
-    }
+		iterableProduct.add(productName);
+		try {
+			Files.write(Paths.get(_FOODFILE), iterableProduct, _options);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void addToSeen(String productName){
-      // Add to SEEN file
-      ArrayList<String> iterableProduct = new ArrayList<String>();
-      iterableProduct.add(productName);
-      try{
-        Files.write(Paths.get(SEENFILE), iterableProduct, options);
-      } catch(Exception e){
-        e.printStackTrace();
-      }
-    }
+	private void _addToMedicine(String productName) {
 
-    private void addToFood(String productName){
-      // Add to Food file
-      ArrayList<String> iterableProduct = new ArrayList<String>();
-      iterableProduct.add(productName);
-      try{
-        Files.write(Paths.get(FOODFILE), iterableProduct, options);
-      } catch(Exception e){
-        e.printStackTrace();
-      }
-    }
+		// Add to Medicine file
 
-    private void addToMedicine(String productName){
-      // Add to Medicine file
-      ArrayList<String> iterableProduct = new ArrayList<String>();
-      iterableProduct.add(productName);
-      try{
-        Files.write(Paths.get(MEDICINEFILE), iterableProduct, options);
-      } catch(Exception e){
-        e.printStackTrace();
-      }
-    }
- }
+		ArrayList<String> iterableProduct = new ArrayList<>();
+
+		iterableProduct.add(productName);
+		try {
+			Files.write(Paths.get(_MEDICINEFILE), iterableProduct, _options);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void _addToSeen(String productName) {
+
+		// Add to SEEN file
+
+		ArrayList<String> iterableProduct = new ArrayList<>();
+
+		iterableProduct.add(productName);
+		try {
+			Files.write(Paths.get(_SEENFILE), iterableProduct, _options);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean _askIfFood(String productName) {
+		//Ask via commandline if this product is Food
+		System.out.println(
+			"This product has not been seen before. Please answer the " +
+				"following for tax purposes:");
+		boolean answer = false;
+		Console console = null;
+		String response = null;
+
+		try {
+			console = System.console();
+
+			if (console != null) {
+				response = console.readLine("Is " + productName + " food: ");
+
+				if (response.equals("yes") || response.equals("Yes")) {
+					answer = true;
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return answer;
+	}
+
+	private boolean _askIfMedicine(String productName) {
+
+		// Ask via commandline if this product is Medicine
+
+		boolean answer = false;
+		Console console = null;
+		String response = null;
+
+		try {
+			console = System.console();
+
+			if (console != null) {
+				response = console.readLine(
+					"Is " + productName + " medicine: ");
+
+				if (response.equals("yes") || response.equals("Yes")) {
+					answer = true;
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return answer;
+	}
+
+	private boolean _checkIfBook(String productName) {
+		if (productName.equals("book") || productName.equals("books")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _checkIfFood(String productName) {
+
+		// consult Food file
+
+		boolean existsInFood = false;
+
+		try {
+			existsInFood = Files.lines(
+				Paths.get(_FOODFILE)).anyMatch((item) -> item.equals(
+					productName));
+		}
+		catch (IOException e) {
+			//e.printStackTrace();
+			try {
+				Files.createFile(Paths.get(_FOODFILE));
+				_checkIfFood(productName);
+			}
+			catch (IOException x) {
+				x.printStackTrace();
+			}
+		}
+
+		return existsInFood;
+	}
+
+	private boolean _checkIfMedicine(String productName) {
+
+		// Consult MEDICINE file
+
+		boolean existsInMedicine = false;
+
+		try {
+			existsInMedicine = Files.lines(
+				Paths.get(_MEDICINEFILE)).anyMatch((item) -> item.equals(
+					productName));
+		}
+		catch (IOException e) {
+			//e.printStackTrace();
+			try {
+				Files.createFile(Paths.get(_MEDICINEFILE));
+				_checkIfMedicine(productName);
+			}
+			catch (IOException x) {
+				x.printStackTrace();
+			}
+		}
+
+		return existsInMedicine;
+	}
+
+	private boolean _checkIfSeen(String productName) {
+
+		// Consult SEEN file
+
+		boolean existsInSeen = false;
+
+		try {
+			existsInSeen = Files.lines(
+				Paths.get(_SEENFILE)).anyMatch((item) -> item.equals(
+					productName));
+		}
+		catch (IOException e) {
+			//e.printStackTrace();
+			try {
+				Files.createFile(Paths.get(_SEENFILE));
+				_checkIfSeen(productName);
+			}
+			catch (IOException x) {
+				x.printStackTrace();
+			}
+		}
+
+		return existsInSeen;
+	}
+
+	private boolean _checkImportTaxability(String productName) {
+		if (Arrays.asList(productName.split(" ")).contains("imported")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	private static final String _FOODFILE = "food_file.txt";
+
+	private static final String _MEDICINEFILE = "medicine_file.txt";
+
+	private static final String _SEENFILE = "all_seen_products.txt";
+
+	private final boolean _basicSalesTaxApplicable;
+	private final boolean _importSalesTaxApplicable;
+	private final String _name;
+	private final StandardOpenOption[] _options =
+		new StandardOpenOption[] {APPEND, CREATE};
+	private final BigDecimal _postTaxPrice;
+	private final BigDecimal _preTaxPrice;
+	private final int _quantity;
+
+}
--- modules/test/jenkins-results-parser/src/main/java/com/liferay/taxes/CashRegister.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/taxes/CashRegister.java
@@ -10,143 +10,174 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class CashRegister{
+import java.util.Arrays;
 
-  public static final BigDecimal BASICSALESTAX = new BigDecimal("0.10");
-  public static final BigDecimal IMPORTSALESTAX = new BigDecimal("0.05");
-  public static final BigDecimal ROUNDINGVALUE = new BigDecimal("0.05");
+/**
+ * @author Cesar Polanco
+ */
+public class CashRegister {
 
-  public static BigDecimal calculateItemTax(CartItem inItem){
-    BigDecimal itemTax = new BigDecimal("0.00");
-    if(inItem.isTaxable()){
-      itemTax = itemTax.add(calculateTax(inItem.getPreTaxPrice(), BASICSALESTAX));
-    }
+	public static final BigDecimal BASICSALESTAX = new BigDecimal("0.10");
 
-    if(inItem.isImported()){
-      itemTax = itemTax.add(calculateTax(inItem.getPreTaxPrice(), IMPORTSALESTAX));
-    }
+	public static final BigDecimal IMPORTSALESTAX = new BigDecimal("0.05");
 
-    return itemTax;
-  }
+	public static final BigDecimal ROUNDINGVALUE = new BigDecimal("0.05");
 
-  public static BigDecimal roundTax(BigDecimal tax){
-    BigDecimal moduloResult = tax.remainder(ROUNDINGVALUE);
-    if(moduloResult.compareTo(BigDecimal.ZERO) != 0){
-      tax = tax.add(ROUNDINGVALUE.subtract(moduloResult));
-    }
-    return tax;
-  }
+	public static BigDecimal calculateCartTax(Cart customer) {
+		BigDecimal cartTax = new BigDecimal("0.00");
+		BigDecimal itemTax;
 
-  public static BigDecimal scaleTax(BigDecimal tax){
-    return tax.setScale(2, RoundingMode.HALF_UP);
-  }
+		for (CartItem product : customer.contents) {
+			itemTax = calculateItemTax(product);
 
-  public static BigDecimal calculateTax(BigDecimal principal, BigDecimal rate){
-    BigDecimal total = principal.multiply(rate);
-    total = scaleTax(total);
-    total = roundTax(total);
-    return total;
-  }
+			cartTax = cartTax.add(itemTax);
+			product.setPostTaxPrice(itemTax);
+		}
 
-  public static void printReceipt(Cart currentCart){
-    BigDecimal itemTotal;
-    for(CartItem inCart: currentCart.getContents()){
-      itemTotal = new BigDecimal(inCart.getQuantity()).multiply(inCart.getPostTaxPrice()).setScale(2, RoundingMode.HALF_UP);
-      String receiptLine = Integer.toString(inCart.getQuantity()) + " " + inCart.getName() + ": " + itemTotal.toString();
-      System.out.println(receiptLine);
-    }
-    System.out.println("Sales Taxes: " + currentCart.getTotalTax().toString());
-    System.out.println("Total: " + currentCart.getTotal().toString());
-  }
+		return cartTax;
+	}
 
-  public static BigDecimal calculateCartTax(Cart customer){
-    BigDecimal cartTax = new BigDecimal("0.00");
-    BigDecimal itemTax;
-    for(CartItem product: customer.contents){
-      itemTax = calculateItemTax(product);
-      cartTax = cartTax.add(itemTax);
-      product.setPostTaxPrice(itemTax);
-    }
-    return cartTax;
-  }
+	public static BigDecimal calculateItemTax(CartItem inItem) {
+		BigDecimal itemTax = new BigDecimal("0.00");
 
-  public static void main(String[] args){
-    Cart userCart = new Cart();
-    //userCart.addToCart("1 book at 12.49");
-    //userCart.addToCart("1 music CD at 14.99");
-    //userCart.addToCart("1 chocolate bar at 0.85");
+		if (inItem.isTaxable()) {
+			itemTax = itemTax.add(
+				calculateTax(inItem.getPreTaxPrice(), BASICSALESTAX));
+		}
 
-    //userCart.addToCart("1 imported box of chocolates at 10.00");
-    //userCart.addToCart("1 imported bottle of perfume at 47.50");
+		if (inItem.isImported()) {
+			itemTax = itemTax.add(
+				calculateTax(inItem.getPreTaxPrice(), IMPORTSALESTAX));
+		}
 
-    // userCart.addToCart("1 imported bottle of perfume at 27.99");
-    // userCart.addToCart("1 bottle of perfume at 18.99");
-    // userCart.addToCart("1 packet of headache pills at 9.75");
-    // userCart.addToCart("1 imported box of chocolates at 11.25");
-    Console console = null;
-    String inputString = "";
-    boolean interactiveMethod = false;
-    boolean fileMethod = false;
-    String fileName = "";
+		return itemTax;
+	}
 
-    // Differentiate between interactive mode and file mode
-    // Priority goes to -interactive, if both are present
+	public static BigDecimal calculateTax(
+		BigDecimal principal, BigDecimal rate) {
 
-    for(String arg: args){
-      if(arg.equals("-interactive")){
-        interactiveMethod = true;
-      }
-      if(arg.equals("-file")){
-        if( args.length > (Arrays.asList(args).indexOf("-file") + 1)){
-          fileMethod = true;
-          fileName = args[(Arrays.asList(args).indexOf("-file") + 1)];
-        }
-        else{
-          System.out.println("Missing Filename Argument!");
-          System.exit(1);
-        }
-      }
-    }
+		BigDecimal total = principal.multiply(rate);
 
-    if(interactiveMethod){
-      try{
-        console = System.console();
-        System.out.println("Interactive Mode: enter 'Done' to finish the cart");
-        while(!(inputString.equals("Done") || inputString.equals("done"))){
-          if(console != null){
-            inputString = console.readLine("Please enter item: ");
-            if(!(inputString.equals("Done") || inputString.equals("done"))){
-              userCart.addToCart(inputString);
-              System.out.println("Added!");
-            } else {
-              System.out.println("Thank you!");
-            }
-          }
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+		total = scaleTax(total);
+		total = roundTax(total);
 
-      userCart.setTotalTax(calculateCartTax(userCart));
+		return total;
+	}
 
-      userCart.calculateTotal();
-      printReceipt(userCart);
-    }
-    else if(fileMethod){
-      try{
-        Files.lines(Paths.get(fileName)).forEach((line) -> userCart.addToCart(line));
-        userCart.setTotalTax(calculateCartTax(userCart));
-        userCart.calculateTotal();
-        printReceipt(userCart);
-      } catch (IOException e){
-        e.printStackTrace();
-      }
-    }
-    else {
-      System.out.println("Usage: Specify either interactive mode '-interactive' or file mode '-file'\n" +
-                         "File mode requires a filename as an argument\nInteractive Mode takes precedence");
-    }
+	public static void main(String[] args) {
+		Cart userCart = new Cart();
 
-  }
+		Console console = null;
+		String inputString = "";
+		boolean interactiveMethod = false;
+		boolean fileMethod = false;
+		String fileName = "";
+
+		// Differentiate between interactive mode and file mode
+		// Priority goes to -interactive, if both are present
+
+		for (String arg : args) {
+			if (arg.equals("-interactive")) {
+				interactiveMethod = true;
+			}
+
+			if (arg.equals("-file")) {
+				if (args.length > (Arrays.asList(args).indexOf("-file") + 1)) {
+					fileMethod = true;
+					fileName = args[Arrays.asList(args).indexOf("-file") + 1];
+				}
+				else {
+					System.out.println("Missing Filename Argument!");
+					System.exit(1);
+				}
+			}
+		}
+
+		if (interactiveMethod) {
+			try {
+				console = System.console();
+				System.out.println(
+					"Interactive Mode: enter 'Done' to finish the cart");
+				while (!(inputString.equals("Done") ||
+				 inputString.equals("done"))) {
+
+					if (console != null) {
+						inputString = console.readLine("Please enter item: ");
+
+						if (!(inputString.equals("Done") ||
+						 inputString.equals("done"))) {
+
+							userCart.addToCart(inputString);
+							System.out.println("Added!");
+						}
+						else {
+							System.out.println("Thank you!");
+						}
+					}
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			userCart.setTotalTax(calculateCartTax(userCart));
+
+			userCart.calculateTotal();
+			printReceipt(userCart);
+		}
+		else if (fileMethod) {
+			try {
+				Files.lines(
+					Paths.get(fileName)).forEach((line) -> userCart.addToCart(
+					line));
+				userCart.setTotalTax(calculateCartTax(userCart));
+				userCart.calculateTotal();
+				printReceipt(userCart);
+			}
+			catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+		else {
+			System.out.println(
+				"Usage: Specify either interactive mode '-interactive' or " +
+					"file mode '-file'\n File mode requires a filename as an" +
+						"argument\nInteractive Mode takes precedence");
+		}
+	}
+
+	public static void printReceipt(Cart currentCart) {
+		BigDecimal itemTotal;
+
+		for (CartItem inCart : currentCart.getContents()) {
+			itemTotal = new BigDecimal(
+				inCart.getQuantity()).multiply(
+					inCart.getPostTaxPrice()).setScale(2, RoundingMode.HALF_UP);
+
+			String receiptLine =
+				Integer.toString(inCart.getQuantity()) + " " +
+					inCart.getName() + ": " + itemTotal.toString();
+
+			System.out.println(receiptLine);
+		}
+
+		System.out.println(
+			"Sales Taxes: " + currentCart.getTotalTax().toString());
+		System.out.println("Total: " + currentCart.getTotal().toString());
+	}
+
+	public static BigDecimal roundTax(BigDecimal tax) {
+		BigDecimal moduloResult = tax.remainder(ROUNDINGVALUE);
+
+		if (moduloResult.compareTo(BigDecimal.ZERO) != 0) {
+			tax = tax.add(ROUNDINGVALUE.subtract(moduloResult));
+		}
+
+		return tax;
+	}
+
+	public static BigDecimal scaleTax(BigDecimal tax) {
+		return tax.setScale(2, RoundingMode.HALF_UP);
+	}
 
 }
