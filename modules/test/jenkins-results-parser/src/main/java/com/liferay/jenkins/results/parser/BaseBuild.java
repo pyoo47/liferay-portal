78f42cc78c33d41a350187ad7c5c72aa1f4352f0
Kevin Yen <kevin.yen@liferay.com>
2017-06-29 14:19:14 -0700

    LRQA-31884 Add discarded and pending statuses

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
@@ -648,6 +648,12 @@ public abstract class BaseBuild implements Build {
 
 		String status = getStatus();
 
+		if (status.equals("discarded")) {
+			sb.append(" was discarded because its prerequisite build failed.");
+
+			return sb.toString();
+		}
+
 		if (status.equals("completed")) {
 			sb.append(" completed at ");
 			sb.append(getBuildURL());
@@ -661,6 +667,12 @@ public abstract class BaseBuild implements Build {
 			sb.append(" is missing ");
 			sb.append(getJobURL());
 			sb.append(".");
+
+			return sb.toString();
+		}
+
+		if (status.equals("pending")) {
+			sb.append(" is pending.");
 
 			return sb.toString();
 		}
@@ -712,6 +724,12 @@ public abstract class BaseBuild implements Build {
 			" Starting  ", "/ ",
 
 			Integer.toString(getDownstreamBuildCount("missing")), " Missing  ",
+			"/ ",
+
+			Integer.toString(getDownstreamBuildCount("discarded")),
+			" Discarded  ", "/ ",
+
+			Integer.toString(getDownstreamBuildCount("pending")), " Pending  ",
 			"/ ",
 
 			Integer.toString(getDownstreamBuildCount("queued")), " Queued  ",
@@ -1306,6 +1324,12 @@ public abstract class BaseBuild implements Build {
 				return sb.toString();
 			}
 
+			if (status.equals("discarded")) {
+				sb.append(" has been discarded.");
+
+				return sb.toString();
+			}
+
 			if (status.equals("missing")) {
 				sb.append(" is missing ");
 				sb.append(getJobURL());
@@ -1338,6 +1362,12 @@ public abstract class BaseBuild implements Build {
 
 				sb.append(getBuildURL());
 				sb.append(".\n");
+
+				return sb.toString();
+			}
+
+			if (status.equals("pending")) {
+				sb.append(" is pending trigger.");
 
 				return sb.toString();
 			}
@@ -1411,6 +1441,17 @@ public abstract class BaseBuild implements Build {
 	}
 
 	protected abstract Element getGitHubMessageJobResultsElement();
+
+	protected String getInvocationMessage() {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("Invoked: ");
+		sb.append(getJobName());
+		sb.append(" at ");
+		sb.append(getInvocationURL());
+
+		return sb.toString();
+	}
 
 	protected Set<String> getJobParameterNames() {
 		JSONObject jsonObject;
