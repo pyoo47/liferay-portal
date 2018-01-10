701645389704c3eec755dbcbac648bbbbd2dfa58
Kenji Heigel <kenji.heigel@liferay.com>
2018-01-09 18:00:13 -0800

    LRQA-36241 Clarify github message when there are only upstream failures and nothing unique

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuild.java
@@ -1108,11 +1108,6 @@ public class TopLevelBuild extends BaseBuild {
 				UpstreamFailureUtil.loadUpstreamJobFailuresJSONObject(this);
 			}
 
-			Dom4JUtil.addToElement(
-				rootElement, Dom4JUtil.getNewElement("hr"),
-				Dom4JUtil.getNewElement(
-					"h4", null, "Failures unique to this pull:"));
-
 			Map<Build, Element> downstreamBuildFailureMessages =
 				getDownstreamBuildMessages("ABORTED", "FAILURE", "UNSTABLE");
 
@@ -1156,6 +1151,23 @@ public class TopLevelBuild extends BaseBuild {
 			}
 
 			failureElements.add(0, super.getGitHubMessageElement());
+
+			Dom4JUtil.addToElement(rootElement, Dom4JUtil.getNewElement("hr"));
+
+			if ((failureElements.size() == 1) &&
+				(upstreamJobFailureElements.size() > 0)) {
+
+				Dom4JUtil.addToElement(
+					rootElement,
+					Dom4JUtil.getNewElement(
+						"h4", null, "This pull contains no unique failures."));
+			}
+			else {
+				Dom4JUtil.addToElement(
+					rootElement,
+					Dom4JUtil.getNewElement(
+						"h4", null, "Failures unique to this pull:"));
+			}
 
 			Dom4JUtil.getOrderedListElement(
 				failureElements, rootElement, maxFailureCount);
