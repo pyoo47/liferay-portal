9dad5da1d6b1e7244e563ff7a37721fbd51c0d59
Kenji Heigel <kenji.heigel@liferay.com>
2018-01-11 14:18:27 -0800

    LRQA-37192 Only show top level failure section if it's the only failure

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuild.java
@@ -1150,7 +1150,9 @@ public class TopLevelBuild extends BaseBuild {
 				}
 			}
 
-			failureElements.add(0, super.getGitHubMessageElement());
+			if (failureElements.isEmpty()) {
+				failureElements.add(0, super.getGitHubMessageElement());
+			}
 
 			Dom4JUtil.addToElement(rootElement, Dom4JUtil.getNewElement("hr"));
 
