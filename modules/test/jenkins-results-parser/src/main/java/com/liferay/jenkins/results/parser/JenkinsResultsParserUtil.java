e82e3b7fe54557c66caf2584960939cc86b8606e
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-03-12 11:29:19 -0700

    LRQA-39077 Add a Label class to interact with Github labels

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -475,6 +475,7 @@ public class JenkinsResultsParserUtil {
 	}
 
 	public static String fixURL(String url) {
+		url = url.replace(" ", "%20");
 		url = url.replace("#", "%23");
 		url = url.replace("(", "%28");
 		url = url.replace(")", "%29");
