e4ef5140ce7d71e042209ac39ab279c2a73991f3
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-04-11 13:45:31 -0700

    LRQA-39324 Compare the test suite names with Objects#equals

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BatchTestClassGroup.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -111,15 +112,13 @@ public abstract class BatchTestClassGroup extends BaseTestClassGroup {
 					continue;
 				}
 
-				if (testSuiteName == null) {
-					continue;
+				String targetTestSuiteName = matcher.group("testSuiteName");
+
+				if (Objects.equals(testSuiteName, targetTestSuiteName)) {
+					return wildcardPropertyName;
 				}
 
-				if (!testSuiteName.equals(matcher.group("testSuiteName"))) {
-					continue;
-				}
-
-				return wildcardPropertyName;
+				continue;
 			}
 		}
 
