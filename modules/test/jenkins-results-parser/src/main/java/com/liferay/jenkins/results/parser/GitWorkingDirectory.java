94df7402fdcf440aed8c4dd3d5b27698d02b3da6
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-08-02 17:30:42 -0700

    LRQA-42795 Add ability to run these build runners outside of CI

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitWorkingDirectory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitWorkingDirectory.java
@@ -1378,6 +1378,13 @@ public class GitWorkingDirectory {
 		return _upstreamBranchName;
 	}
 
+	public RemoteGitBranch getUpstreamRemoteGitBranch() {
+		return getRemoteGitBranch(
+			getUpstreamBranchName(),
+			JenkinsResultsParserUtil.combine(
+				"git@github.com:liferay/", getRepositoryName()));
+	}
+
 	public File getWorkingDirectory() {
 		return _workingDirectory;
 	}
