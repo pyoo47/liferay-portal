5858d888824cee40f38a00ec25f73d8c126874a3
pyoo47 <peter.yoo@liferay.com>
2018-08-03 10:37:11 -0700

    LRQA-42795 Move remoteURLPattern and gitLsRemotePattern into BaseGitRemote

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitWorkingDirectory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitWorkingDirectory.java
@@ -584,7 +584,7 @@ public class GitWorkingDirectory {
 			throw new IllegalArgumentException("Remote URL is null");
 		}
 
-		Matcher remoteURLMatcher = _remoteURLPattern.matcher(remoteURL);
+		Matcher remoteURLMatcher = Remote.remoteURLPattern.matcher(remoteURL);
 
 		if (!remoteURLMatcher.find()) {
 			throw new IllegalArgumentException(
@@ -1150,7 +1150,7 @@ public class GitWorkingDirectory {
 	public List<RemoteGitBranch> getRemoteGitBranches(
 		String remoteGitBranchName, String remoteURL) {
 
-		Matcher remoteURLMatcher = _remoteURLPattern.matcher(remoteURL);
+		Matcher remoteURLMatcher = Remote.remoteURLPattern.matcher(remoteURL);
 
 		if (!remoteURLMatcher.find()) {
 			throw new IllegalArgumentException(
@@ -1189,7 +1189,9 @@ public class GitWorkingDirectory {
 				remoteURLMatcher.group("username"));
 
 		for (String line : input.split("\n")) {
-			Matcher gitLsRemoteMatcher = _gitLsRemotePattern.matcher(line);
+			Pattern gitLsRemotePattern = Remote.gitLsRemotePattern;
+
+			Matcher gitLsRemoteMatcher = gitLsRemotePattern.matcher(line);
 
 			if (gitLsRemoteMatcher.find()) {
 				remoteGitBranches.add(
@@ -1254,7 +1256,8 @@ public class GitWorkingDirectory {
 			throw new IllegalArgumentException("Remote URL is null");
 		}
 
-		Matcher remoteURLMatcher = _remoteURLPattern.matcher(remoteURL);
+		Matcher remoteURLMatcher = BaseGitRemote.remoteURLPattern.matcher(
+			remoteURL);
 
 		if (!remoteURLMatcher.find()) {
 			throw new IllegalArgumentException(
@@ -1278,7 +1281,7 @@ public class GitWorkingDirectory {
 		String input = executionResult.getStandardOut();
 
 		for (String line : input.split("\n")) {
-			Matcher matcher = _gitLsRemotePattern.matcher(line);
+			Matcher matcher = BaseGitRemote.gitLsRemotePattern.matcher(line);
 
 			if (matcher.find()) {
 				return matcher.group("sha");
@@ -1496,7 +1499,8 @@ public class GitWorkingDirectory {
 			throw new IllegalArgumentException("Remote URL is null");
 		}
 
-		Matcher remoteURLMatcher = _remoteURLPattern.matcher(remoteURL);
+		Matcher remoteURLMatcher = BaseGitRemote.remoteURLPattern.matcher(
+			remoteURL);
 
 		if (!remoteURLMatcher.find()) {
 			throw new IllegalArgumentException(
@@ -2101,18 +2105,12 @@ public class GitWorkingDirectory {
 		"gitdir\\: (.*)\\s*");
 	private static final Pattern _gitLogEntityPattern = Pattern.compile(
 		"(?<sha>[0-9a-f]{40}) (?<message>.*)");
-	private static final Pattern _gitLsRemotePattern = Pattern.compile(
-		"(?<sha>[^\\s]{40}+)[\\s]+refs/heads/(?<name>[^\\s]+)");
 	private static final List<String> _privateOnlyRepositoryNames =
 		_getBuildPropertyAsList(
 			"git.working.directory.private.only.repository.names");
 	private static final List<String> _publicOnlyRepositoryNames =
 		_getBuildPropertyAsList(
 			"git.working.directory.public.only.repository.names");
-	private static final Pattern _remoteURLPattern = Pattern.compile(
-		JenkinsResultsParserUtil.combine(
-			"git@(?<hostname>[^:]+):(?<username>[^/]+)/",
-			"(?<repositoryName>[^\\.]+)(.git)?"));
 
 	private File _gitDirectory;
 	private Set<String> _javaDirPaths;
