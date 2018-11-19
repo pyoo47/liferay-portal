5d93577f4dfd386c4e612dd8d8f461b3e4d6a991
pyoo47 <peter.yoo@liferay.com>
2018-11-19 15:08:40 -0800

    LRQA-44833 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -1490,23 +1490,6 @@ public class JenkinsResultsParserUtil {
 		return false;
 	}
 
-	public static boolean isReachable(String hostname) {
-		try {
-			InetAddress inetAddress = InetAddress.getByName(hostname);
-
-			if (inetAddress.isReachable(5000)) {
-				return true;
-			}
-		}
-		catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-
-		System.out.println("Unable to reach " + hostname);
-
-		return false;
-	}
-
 	public static boolean isJSONArrayEqual(
 		JSONArray expectedJSONArray, JSONArray actualJSONArray) {
 
@@ -1571,6 +1554,23 @@ public class JenkinsResultsParserUtil {
 		}
 
 		return true;
+	}
+
+	public static boolean isReachable(String hostname) {
+		try {
+			InetAddress inetAddress = InetAddress.getByName(hostname);
+
+			if (inetAddress.isReachable(5000)) {
+				return true;
+			}
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+		System.out.println("Unable to reach " + hostname);
+
+		return false;
 	}
 
 	public static boolean isWindows() {
