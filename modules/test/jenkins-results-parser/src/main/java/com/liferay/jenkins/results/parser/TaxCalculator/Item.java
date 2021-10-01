dd5e9e962273459187eb386bd08e549b4c70ec7b
Charlotte Wong <chiharutaru@gmail.com>
2021-10-01 15:32:12 -0700

    LRCI-2534 Source formatting.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TaxCalculator/Item.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TaxCalculator/Item.java
@@ -1,51 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.jenkins.results.parser.TaxCalculator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author Charlotte Wong
+ */
 public class Item {
-    private int amount;
-    private String name;
-    private double price;
-    private boolean imported;
-    private boolean exempt;
 
-    public Item(int amount, String name, double price) {
-        this.amount = amount;
-        this.name = name;
-        this.price = price;
-        this.imported = this.checkImport();
-        this.exempt = this.checkExempt();
-    }
+	public Item(int amount, String name, double price) {
+		_amount = amount;
+		_name = name;
+		_price = price;
 
-    public int getAmount() {
-        return this.amount;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public double getPrice() {
-        return this.price;
-    }
-    public boolean isImported() {
-        return this.imported;
-    }
-    public boolean isExempt() {
-        return this.exempt;
-    }
+		_exemptPattern = Pattern.compile("chocolate|book|pill");
 
-    private boolean checkImport() {
-        if (this.name.indexOf("imported") >= 0) {
-            return true;
-        }
-        return false;
-    }
+		_imported = _checkImport();
+		_exempt = _checkExempt();
+	}
 
-    private boolean checkExempt() {
-        Matcher matcher = Pattern.compile("chocolate|book|pill").matcher(this.name);
-        if (matcher.find()) {
-            return true;
-        }
-        return false;
-    }
+	public int getAmount() {
+		return _amount;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public double getPrice() {
+		return _price;
+	}
+
+	public boolean isExempt() {
+		return _exempt;
+	}
+
+	public boolean isImported() {
+		return _imported;
+	}
+
+	private boolean _checkExempt() {
+		Matcher matcher = _exemptPattern.matcher(_name);
+
+		if (matcher.find()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _checkImport() {
+		if (_name.indexOf("imported") >= 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private final int _amount;
+	private final boolean _exempt;
+	private final Pattern _exemptPattern;
+	private final boolean _imported;
+	private final String _name;
+	private final double _price;
+
 }
--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TaxCalculator/Tax.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TaxCalculator/Tax.java
@@ -1,113 +1,154 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.jenkins.results.parser.TaxCalculator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import java.lang.Math;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Scanner;
 
+/**
+ * @author Charlotte Wong
+ */
 public class Tax {
-    private ArrayList<Item> items;
 
-    public Tax() {
-        items = new ArrayList<Item>();
-    }
+	public static void main(String[] args) {
+		Tax testTax = new Tax();
 
-    public double calculateTax(Item item) {
-        double tax = 0.0;
-        if (!item.isExempt()) {
-            tax += item.getPrice() * .10;
-        }
-        if (item.isImported()) {
-            tax += item.getPrice() * .05;
-        }
+		testTax.getInput();
+		testTax.print();
+	}
 
-        tax *= 20;
-        tax = Double.valueOf(Math.round(tax));
-        tax /= 20;
+	public Tax() {
+		_digitPattern = Pattern.compile("\\d+");
+		_resourceFolderURL = "/com/liferay/jenkins/results/parser/dependencies";
+	}
 
-        return tax;
-    }
+	public double calculateTax(Item item) {
+		double tax = 0.0;
 
-    public void getInput() {
-        try {
-            Class<?> clazz = Tax.class;
-            URL resourceURL = clazz.getResource("/com/liferay/jenkins/results/parser/dependencies/TaxCalculatorInputs/input3.txt");
-            System.out.println("resource url: " + resourceURL);
-            URI resourceURI = resourceURL.toURI();
+		if (!item.isExempt()) {
+			tax += item.getPrice() * .10;
+		}
 
-            Path resourcePath = Paths.get(resourceURI);
-            File resourceFile = resourcePath.toFile();
-            String path = resourceFile.getPath();
+		if (item.isImported()) {
+			tax += item.getPrice() * .05;
+		}
 
-            File file = new File(path);
-            Scanner scanner = new Scanner(file);
+		tax *= 20;
 
-            String input = "";
+		tax = Double.valueOf(Math.round(tax));
 
-            while (scanner.hasNextLine()){
-                input = scanner.nextLine();
-                this.parseInput(input);
-            }
-        } catch (FileNotFoundException | URISyntaxException fileNotFoundException) {
-            System.err.println("Error, file not found.");
-            fileNotFoundException.printStackTrace();
-        }
-    }
+		tax /= 20;
 
-    public void parseInput(String input) {
-        int atIndex = input.indexOf(" at ");
-        if (atIndex == -1) {
-            System.err.println("Invalid input.");
-            return;
-        }
-        if (!Character.isDigit(input.charAt(0))) {
-            System.err.println("Invalid input.");
-            return;
-        }
+		return tax;
+	}
 
-        Matcher matcher = Pattern.compile("\\d+").matcher(input);
-        matcher.find();
+	public void getInput() {
+		try {
+			Class<?> clazz = Tax.class;
 
-        String amountString = matcher.group();
-        int amountInt = Integer.valueOf(amountString);
+			URL resourceURL = clazz.getResource(
+				_resourceFolderURL + "/TaxCalculatorInputs/input3.txt");
 
-        String itemName = input.substring(amountString.length() + 1, atIndex);
-        String itemPrice = input.substring(atIndex + 4);
-        Double itemPriceDouble = Double.parseDouble(itemPrice);
+			URI resourceURI = resourceURL.toURI();
 
-        this.items.add(new Item(amountInt, itemName, itemPriceDouble));
+			Path resourcePath = Paths.get(resourceURI);
 
-        return;
-    }
+			File resourceFile = resourcePath.toFile();
 
-    public void print() {
-        double total = 0.0;
-        double totalTax = 0.0;
-        for (int i = 0; i < items.size(); i++) {
-            double itemTax = calculateTax(items.get(i));
-            double itemCostWithTax = items.get(i).getPrice() + itemTax;
+			String path = resourceFile.getPath();
 
-            System.out.println(items.get(i).getAmount() + " " + items.get(i).getName() + ": " + String.format("%.2f", itemCostWithTax));
-            totalTax += itemTax;
-            total += itemCostWithTax;
-        }
-        System.out.println("Sales Taxes: " + String.format("%.2f", totalTax));
-        System.out.println("Total: " + String.format("%.2f", total) );
-    }
+			File file = new File(path);
 
-    public static void main(String[] args) {
-        Tax testTax = new Tax();
-        testTax.getInput();
-        testTax.print();
-    }
+			Scanner scanner = new Scanner(file);
+
+			String input = "";
+
+			while (scanner.hasNextLine()) {
+				input = scanner.nextLine();
+
+				parseInput(input);
+			}
+		}
+		catch (FileNotFoundException | URISyntaxException exception) {
+			System.err.println("Error, file not found.");
+			exception.printStackTrace();
+		}
+	}
+
+	public void parseInput(String input) {
+		int atIndex = input.indexOf(" at ");
+
+		if ((atIndex == -1) || !Character.isDigit(input.charAt(0))) {
+			System.err.println("Invalid input.");
+
+			return;
+		}
+
+		Matcher matcher = _digitPattern.matcher(input);
+
+		matcher.find();
+
+		String amountString = matcher.group();
+
+		int amountInt = Integer.valueOf(amountString);
+
+		String itemName = input.substring(amountString.length() + 1, atIndex);
+
+		String itemPrice = input.substring(atIndex + 4);
+
+		Double itemPriceDouble = Double.parseDouble(itemPrice);
+
+		_items.add(new Item(amountInt, itemName, itemPriceDouble));
+	}
+
+	public void print() {
+		double total = 0.0;
+		double totalTax = 0.0;
+
+		for (Item item : _items) {
+			double itemTax = calculateTax(item);
+
+			double itemCostWithTax = item.getPrice() + itemTax;
+
+			System.out.println(
+				item.getAmount() + " " + item.getName() + ": " +
+					String.format("%.2f", itemCostWithTax));
+
+			totalTax += itemTax;
+
+			total += itemCostWithTax;
+		}
+
+		System.out.println("Sales Taxes: " + String.format("%.2f", totalTax));
+		System.out.println("Total: " + String.format("%.2f", total));
+	}
+
+	private final Pattern _digitPattern;
+	private final ArrayList<Item> _items = new ArrayList<>();
+	private final String _resourceFolderURL;
+
 }
