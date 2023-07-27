7f277f45b7d5deafcba673ffbd158f20b96571c2
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-07-26 18:12:01 -0700

    LRCI-3710 Add ProjectsToJenkinsCohortsDALO class

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/project/dalo/ProjectsToJenkinsCohortsDALO.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/project/dalo/ProjectsToJenkinsCohortsDALO.java
@@ -3,14 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.jethr0.jenkins.dalo;
+package com.liferay.jethr0.project.dalo;
 
 import com.liferay.jethr0.entity.dalo.BaseEntityRelationshipDALO;
 import com.liferay.jethr0.entity.factory.EntityFactory;
 import com.liferay.jethr0.jenkins.cohort.JenkinsCohort;
 import com.liferay.jethr0.jenkins.cohort.JenkinsCohortFactory;
-import com.liferay.jethr0.jenkins.server.JenkinsServer;
-import com.liferay.jethr0.jenkins.server.JenkinsServerFactory;
+import com.liferay.jethr0.project.Project;
+import com.liferay.jethr0.project.ProjectFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -19,28 +19,28 @@ import org.springframework.context.annotation.Configuration;
  * @author Michael Hashimoto
  */
 @Configuration
-public class JenkinsCohortToJenkinsServersDALO
-	extends BaseEntityRelationshipDALO<JenkinsCohort, JenkinsServer> {
+public class ProjectsToJenkinsCohortsDALO
+	extends BaseEntityRelationshipDALO<Project, JenkinsCohort> {
 
 	@Override
-	public EntityFactory<JenkinsServer> getChildEntityFactory() {
-		return _jenkinsServerFactory;
-	}
-
-	@Override
-	public EntityFactory<JenkinsCohort> getParentEntityFactory() {
+	public EntityFactory<JenkinsCohort> getChildEntityFactory() {
 		return _jenkinsCohortFactory;
 	}
 
 	@Override
+	public EntityFactory<Project> getParentEntityFactory() {
+		return _projectFactory;
+	}
+
+	@Override
 	protected String getObjectRelationshipName() {
-		return "jenkinsCohortToJenkinsServers";
+		return "projectsToJenkinsCohorts";
 	}
 
 	@Autowired
 	private JenkinsCohortFactory _jenkinsCohortFactory;
 
 	@Autowired
-	private JenkinsServerFactory _jenkinsServerFactory;
+	private ProjectFactory _projectFactory;
 
 }
