a77ab0673fb0c5efdc2e9806a51b6130e79f2a25
Peter Yoo <peter.yoo@liferay.com>
2023-08-25 11:24:13 -0700

    LRCI-3671 Implement GitHubSecondaryRateLimitRuntimeException

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitHubSecondaryRateLimitRuntimeException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+/**
+ * @author Peter Yoo
+ */
+public class GitHubSecondaryRateLimitRuntimeException extends RuntimeException {
+
+	public GitHubSecondaryRateLimitRuntimeException(Exception exception) {
+		super(exception);
+
+		_gitHubSecondaryRateLimitIOException =
+			(GitHubSecondaryRateLimitIOException)exception;
+	}
+
+	public GitHubSecondaryRateLimitIOException
+		getGitHubSecondaryRateLimitIOException() {
+
+		return _gitHubSecondaryRateLimitIOException;
+	}
+
+	private final GitHubSecondaryRateLimitIOException
+		_gitHubSecondaryRateLimitIOException;
+
+}
