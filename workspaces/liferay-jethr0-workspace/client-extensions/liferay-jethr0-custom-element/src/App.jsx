819c73248847be0a5ff5fdb741bbe61138dcd004
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-08-31 14:50:16 -0700

    LRCI-3766 Add a not found page

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-custom-element/src/App.jsx
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-custom-element/src/App.jsx
@@ -4,6 +4,7 @@
  */
 
 import HomePage from './pages/HomePage/HomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 import {HashRouter, Routes, Route} from "react-router-dom";
 
@@ -14,6 +15,7 @@ function App() {
 		<HashRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</HashRouter>
 	);
