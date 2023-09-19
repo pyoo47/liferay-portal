7a79b48417575673169099fb0972c928ab4131f9
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-09-18 18:58:34 -0700

    LRCI-3789 Add a portal pull request sf job implemenation

---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/job/PortalPullRequestSFJobEntity.java
@@ -0,0 +1,74 @@
+package com.liferay.jethr0.job;
+
+import com.liferay.jethr0.bui1d.BuildEntity;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PortalPullRequestSFJobEntity extends BaseJobEntity {
+
+	@Override
+	public List<JSONObject> getInitialBuildJSONObjects() {
+		List<JSONObject> initialBuildJSONObjects = new ArrayList<>();
+
+		initialBuildJSONObjects.add(_getInitialBuildJSONObject());
+
+		return initialBuildJSONObjects;
+	}
+
+	protected PortalPullRequestSFJobEntity(JSONObject jsonObject) {
+		super(jsonObject);
+
+		_initialBuildParameters.put("BUILD_PRIORITY", _BUILD_PRIORITY);
+		_initialBuildParameters.put(
+			"PULL_REQUEST_URL", jsonObject.optString("pullRequestURL"));
+	}
+
+	private JSONObject _getInitialBuildJSONObject() {
+		JSONObject initialBuildJSONObject = new JSONObject();
+
+		initialBuildJSONObject.put(
+			"buildParameters", _getInitialBuildParametersJSONArray()
+		).put(
+			"initialBuild", true
+		).put(
+			"jenkinsJobName", _JENKINS_JOB_NAME
+		).put(
+			"name", _JENKINS_JOB_NAME
+		).put(
+			"state", BuildEntity.State.OPENED
+		);
+
+		return initialBuildJSONObject;
+	}
+
+	private JSONArray _getInitialBuildParametersJSONArray() {
+		JSONArray initialBuildParametersJSONArray = new JSONArray();
+
+		for (Map.Entry<String, String> initialBuildParameter :
+				_initialBuildParameters.entrySet()) {
+
+			JSONObject initialBuildParameterJSONObject = new JSONObject();
+
+			initialBuildParameterJSONObject.put(
+				"name", initialBuildParameter.getKey()
+			).put(
+				"value", initialBuildParameter.getValue()
+			);
+
+			initialBuildParametersJSONArray.put(initialBuildParameterJSONObject);
+		}
+
+		return initialBuildParametersJSONArray;
+	}
+
+	private static final String _BUILD_PRIORITY = "4";
+	private static final String _JENKINS_JOB_NAME = "test-portal-source-format";
+
+	private final Map<String, String> _initialBuildParameters = new HashMap<>();
+
+}
