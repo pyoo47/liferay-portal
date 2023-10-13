42137b76d21f6331ad46ce4e2e02c76fe6ac0ff0
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-10-12 23:33:49 -0700

    LRCI-3821 Add BuildUpdater class hierarchy

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuildUpdater.java
@@ -0,0 +1,312 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+/**
+ * @author Michael Hashimoto
+ */
+public abstract class BaseBuildUpdater implements BuildUpdater {
+
+	@Override
+	public Build getBuild() {
+		return _build;
+	}
+
+	@Override
+	public void update() {
+		String status = _build.getStatus();
+
+		if (status.equals("completed")) {
+			runCompleted();
+		}
+		else if (status.equals("missing")) {
+			runMissing();
+		}
+		else if (status.equals("queued")) {
+			runQueued();
+		}
+		else if (status.equals("reporting")) {
+			runReporting();
+		}
+		else if (status.equals("running")) {
+			runRunning();
+		}
+		else if (status.equals("starting")) {
+			runStarting();
+		}
+	}
+
+	protected BaseBuildUpdater(Build build) {
+		_build = build;
+	}
+
+	protected abstract boolean isBuildCompleted();
+
+	protected abstract boolean isBuildQueued();
+
+	protected abstract boolean isBuildRunning();
+
+	protected void runCompleted() {
+		_build.setStatus("completed");
+	}
+
+	protected void runMissing() {
+		_build.setStatus("missing");
+
+		if (isBuildQueued()) {
+			runQueued();
+
+			return;
+		}
+
+		if (isBuildRunning()) {
+			runRunning();
+
+			return;
+		}
+
+		runReporting();
+	}
+
+	protected void runQueued() {
+		_build.setStatus("queued");
+
+		if (isBuildQueued()) {
+			return;
+		}
+
+		if (isBuildRunning()) {
+			runRunning();
+
+			return;
+		}
+
+		_build.setStatus("missing");
+	}
+
+	protected void runReporting() {
+		_build.setStatus("reporting");
+
+		if (!isBuildCompleted()) {
+			return;
+		}
+
+		_isApplySlaveOfflineRules();
+
+		if (_isApplyReinvokeRules() ||
+			(_build.isFailing() && !_build.hasMaximumInvocationCount())) {
+
+			_build.setStatus("starting");
+
+			return;
+		}
+
+		runCompleted();
+	}
+
+	protected void runRunning() {
+		_build.setStatus("running");
+
+		if (!isBuildCompleted()) {
+			return;
+		}
+
+		runReporting();
+	}
+
+	protected void runStarting() {
+		_build.setStatus("starting");
+
+		_build.reset();
+
+		Build.Invocation previousInvocation = _build.getPreviousInvocation();
+
+		if (previousInvocation != null) {
+			reinvoke();
+		}
+		else {
+			invoke();
+		}
+
+		runQueued();
+	}
+
+	private boolean _isApplyReinvokeRules() {
+		Build build = getBuild();
+
+		if (build instanceof AxisBuild || build instanceof ParentBuild) {
+			return false;
+		}
+
+		if ((build.isCompleted() && !build.isFailing()) ||
+			!build.isCompleted() || build.isFromArchive() ||
+			build.hasMaximumInvocationCount()) {
+
+			return false;
+		}
+
+		for (ReinvokeRule reinvokeRule : ReinvokeRule.getReinvokeRules()) {
+			if (!reinvokeRule.matches(build)) {
+				continue;
+			}
+
+			_reinvoke(reinvokeRule);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isApplySlaveOfflineRules() {
+		Build build = getBuild();
+
+		if (build instanceof BatchBuild) {
+			return false;
+		}
+
+		if ((build.isCompleted() && !build.isFailing()) ||
+			!build.isCompleted() || build.isFromArchive()) {
+
+			return false;
+		}
+
+		JenkinsSlave jenkinsSlave = build.getJenkinsSlave();
+
+		if (jenkinsSlave == null) {
+			return false;
+		}
+
+		jenkinsSlave.update();
+
+		if (jenkinsSlave.isOffline()) {
+			return false;
+		}
+
+		for (SlaveOfflineRule slaveOfflineRule :
+				SlaveOfflineRule.getSlaveOfflineRules()) {
+
+			if (!slaveOfflineRule.matches(build)) {
+				continue;
+			}
+
+			_takeSlaveOffline(slaveOfflineRule);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private void _reinvoke(ReinvokeRule reinvokeRule) {
+		Build build = getBuild();
+
+		if (build instanceof AxisBuild || build instanceof ParentBuild ||
+			build.hasMaximumInvocationCount()) {
+
+			return;
+		}
+
+		Build parentBuild = build.getParentBuild();
+
+		if (parentBuild == null) {
+			return;
+		}
+
+		String parentBuildStatus = parentBuild.getStatus();
+
+		if (!parentBuildStatus.equals("running") ||
+			!JenkinsResultsParserUtil.isCINode() ||
+			build.isFromCompletedBuild()) {
+
+			return;
+		}
+
+		if ((reinvokeRule != null) && !build.isFromArchive()) {
+			String message = JenkinsResultsParserUtil.combine(
+				reinvokeRule.getName(), " failure detected at ",
+				build.getBuildURL(), ". This build will be reinvoked.\n\n",
+				reinvokeRule.toString(), "\n\n");
+
+			System.out.println(message);
+
+			TopLevelBuild topLevelBuild = build.getTopLevelBuild();
+
+			if (topLevelBuild != null) {
+				message = JenkinsResultsParserUtil.combine(
+					message, "Top Level Build URL: ",
+					topLevelBuild.getBuildURL());
+			}
+
+			String notificationRecipients =
+				reinvokeRule.getNotificationRecipients();
+
+			if ((notificationRecipients != null) &&
+				!notificationRecipients.isEmpty()) {
+
+				NotificationUtil.sendEmail(
+					message, "jenkins", "Build Reinvoked",
+					reinvokeRule.notificationRecipients);
+			}
+		}
+
+		reinvoke();
+	}
+
+	private void _takeSlaveOffline(SlaveOfflineRule slaveOfflineRule) {
+		Build build = getBuild();
+
+		if ((slaveOfflineRule == null) || build.isFromArchive()) {
+			return;
+		}
+
+		String pinnedMessage = "";
+
+		if (!slaveOfflineRule.shutdown) {
+			pinnedMessage = "PINNED\n";
+		}
+
+		JenkinsSlave jenkinsSlave = build.getJenkinsSlave();
+
+		JenkinsMaster jenkinsMaster = jenkinsSlave.getJenkinsMaster();
+
+		String slaveOfflineRuleString = slaveOfflineRule.toString();
+
+		slaveOfflineRuleString = slaveOfflineRuleString.replace("\\", "\\\\");
+
+		String message = JenkinsResultsParserUtil.combine(
+			pinnedMessage, slaveOfflineRule.getName(), " failure detected at ",
+			build.getBuildURL(), ". ", jenkinsSlave.getName(),
+			" will be taken offline.\n\n", slaveOfflineRuleString,
+			"\n\n\nOffline Slave URL: https://", jenkinsMaster.getName(),
+			".liferay.com/computer/", jenkinsSlave.getName(), "\n");
+
+		System.out.println(message);
+
+		TopLevelBuild topLevelBuild = build.getTopLevelBuild();
+
+		if (topLevelBuild != null) {
+			message = JenkinsResultsParserUtil.combine(
+				message, "Top Level Build URL: ", topLevelBuild.getBuildURL());
+		}
+
+		jenkinsSlave.takeSlavesOffline(message);
+
+		String notificationRecipients =
+			slaveOfflineRule.getNotificationRecipients();
+
+		if ((notificationRecipients != null) &&
+			!notificationRecipients.isEmpty()) {
+
+			NotificationUtil.sendEmail(
+				message, "jenkins", "Slave Offline",
+				slaveOfflineRule.notificationRecipients);
+		}
+	}
+
+	private final Build _build;
+
+}
---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/DefaultBuildUpdater.java
@@ -0,0 +1,261 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.IOException;
+
+import java.util.Objects;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class DefaultBuildUpdater extends BaseBuildUpdater {
+
+	@Override
+	public void invoke() {
+		Build build = getBuild();
+
+		_invoke(
+			build.getInvokedBatchSize(), build.getMinimumSlaveRAM(),
+			build.getMaximumSlavesPerHost());
+	}
+
+	@Override
+	public void reinvoke() {
+		Build build = getBuild();
+
+		_invoke(
+			build.getInvokedBatchSize(), 24, build.getMaximumSlavesPerHost());
+	}
+
+	protected DefaultBuildUpdater(Build build) {
+		super(build);
+	}
+
+	@Override
+	protected boolean isBuildCompleted() {
+		Build build = getBuild();
+
+		if (!_isBuildCompleted(build)) {
+			return false;
+		}
+
+		if (build instanceof ParentBuild) {
+			ParentBuild parentBuild = (ParentBuild)build;
+
+			for (Build downstreamBuild : parentBuild.getDownstreamBuilds()) {
+				if (!_isBuildCompleted(downstreamBuild)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	protected boolean isBuildQueued() {
+		try {
+			JSONObject queueItemJSONObject = _getQueueItemJSONObject();
+
+			if (queueItemJSONObject == null) {
+				return false;
+			}
+
+			return true;
+		}
+		catch (Exception exception) {
+			Build build = getBuild();
+
+			System.out.println(
+				JenkinsResultsParserUtil.combine(
+					"[", build.getBuildName(), "] Unable to get queue item"));
+		}
+
+		return false;
+	}
+
+	protected boolean isBuildRunning() {
+		try {
+			JSONObject runningBuildJSONObject = _getRunningBuildJSONObject();
+
+			if (runningBuildJSONObject == null) {
+				return false;
+			}
+
+			Build build = getBuild();
+
+			build.setBuildURL(runningBuildJSONObject.getString("url"));
+
+			return true;
+		}
+		catch (Exception exception) {
+			exception.printStackTrace();
+
+			Build build = getBuild();
+
+			System.out.println(
+				JenkinsResultsParserUtil.combine(
+					"[", build.getBuildName(), "] Unable to get build item"));
+		}
+
+		return false;
+	}
+
+	private JSONObject _getQueueItemJSONObject() {
+		try {
+			Build build = getBuild();
+
+			JenkinsMaster jenkinsMaster = build.getJenkinsMaster();
+
+			if (jenkinsMaster == null) {
+				return null;
+			}
+
+			JSONObject jsonObject = JenkinsResultsParserUtil.toJSONObject(
+				JenkinsResultsParserUtil.combine(
+					String.valueOf(jenkinsMaster.getURL()),
+					"/queue/api/json?tree=items[id]"),
+				false);
+
+			JSONArray queueItemsJSONArray = jsonObject.getJSONArray("items");
+
+			if (queueItemsJSONArray == null) {
+				return null;
+			}
+
+			Build.Invocation currentInvocation = build.getCurrentInvocation();
+
+			for (int i = 0; i < queueItemsJSONArray.length(); i++) {
+				JSONObject queueItemJSONObject =
+					queueItemsJSONArray.getJSONObject(i);
+
+				if (Objects.equals(
+						queueItemJSONObject.getLong("id"),
+						currentInvocation.getQueueId())) {
+
+					return queueItemJSONObject;
+				}
+			}
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+
+		return null;
+	}
+
+	private JSONObject _getRunningBuildJSONObject() {
+		Build build = getBuild();
+
+		Build.Invocation currentInvocation = build.getCurrentInvocation();
+
+		int page = 0;
+
+		while (true) {
+			JSONArray runningBuildsJSONArray = _getRunningBuildsJSONArray(page);
+
+			if (runningBuildsJSONArray.length() == 0) {
+				break;
+			}
+
+			for (int i = 0; i < runningBuildsJSONArray.length(); i++) {
+				JSONObject runningBuildJSONObject =
+					runningBuildsJSONArray.getJSONObject(i);
+
+				if (Objects.equals(
+						runningBuildJSONObject.getLong("queueId"),
+						currentInvocation.getQueueId())) {
+
+					return runningBuildJSONObject;
+				}
+			}
+
+			page++;
+		}
+
+		return null;
+	}
+
+	private JSONArray _getRunningBuildsJSONArray(final int page) {
+		Retryable<JSONArray> retryable = new Retryable<JSONArray>(
+			true, 2, 10, true) {
+
+			@Override
+			public JSONArray execute() {
+				Build build = getBuild();
+
+				JenkinsMaster jenkinsMaster = build.getJenkinsMaster();
+
+				String url = JenkinsResultsParserUtil.getLocalURL(
+					JenkinsResultsParserUtil.combine(
+						String.valueOf(jenkinsMaster.getURL()), "/job/",
+						JenkinsResultsParserUtil.fixURL(build.getJobName()),
+						"/api/json?tree=allBuilds[queueId,url]{",
+						String.valueOf(page * 100), ",",
+						String.valueOf((page + 1) * 100), "}"));
+
+				try {
+					JSONObject jsonObject =
+						JenkinsResultsParserUtil.toJSONObject(url, false);
+
+					return jsonObject.getJSONArray("allBuilds");
+				}
+				catch (IOException ioException) {
+					throw new RuntimeException(ioException);
+				}
+			}
+
+		};
+
+		return retryable.executeWithRetries();
+	}
+
+	private void _invoke(
+		int invokedBatchSize, int minimumSlaveRAM, int maximumSlavesPerHost) {
+
+		Build build = getBuild();
+
+		JenkinsMaster jenkinsMaster = build.getJenkinsMaster();
+
+		if (jenkinsMaster == null) {
+			JenkinsCohort jenkinsCohort = build.getJenkinsCohort();
+
+			jenkinsMaster = jenkinsCohort.getMostAvailableJenkinsMaster(
+				invokedBatchSize, minimumSlaveRAM, maximumSlavesPerHost);
+
+			build.setJenkinsMaster(jenkinsMaster);
+		}
+
+		JSONObject jsonObject = JenkinsResultsParserUtil.invokeJenkinsBuild(
+			jenkinsMaster, build.getJobName(), build.getParameters());
+
+		build.addInvocation(
+			new Build.Invocation(jenkinsMaster, jsonObject.getLong("queueId")));
+	}
+
+	private boolean _isBuildCompleted(Build build) {
+		JSONObject buildJSONObject = build.getBuildJSONObject(
+			"duration,result");
+
+		if (buildJSONObject == null) {
+			return false;
+		}
+
+		long duration = buildJSONObject.optLong("duration");
+		String result = buildJSONObject.optString("result");
+
+		if ((duration == 0) || JenkinsResultsParserUtil.isNullOrEmpty(result)) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
