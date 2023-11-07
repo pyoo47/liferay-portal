d1871a3de101aeac20f5714f333ed4fbe6eba13e
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-07 14:04:40 -0800

    LRCI-3820 SF

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-sidecar-activemq/data/docker-run.sh
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-sidecar-activemq/data/docker-run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
+# or more contributor license agreements.	See the NOTICE file
 # distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
+# regarding copyright ownership.	The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+# with the License.	You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
+# KIND, either express or implied.	See the License for the
 # specific language governing permissions and limitations
 # under the License.
 
@@ -22,24 +22,36 @@
 set -e
 
 BROKER_HOME=/var/lib/
+
 CONFIG_PATH=$BROKER_HOME/etc
+
 export BROKER_HOME OVERRIDE_PATH CONFIG_PATH
 
-if [[ ${ANONYMOUS_LOGIN,,} == "true" ]]; then
-  LOGIN_OPTION="--allow-anonymous"
+if [[ ${ANONYMOUS_LOGIN,,} == "true" ]]
+then
+	LOGIN_OPTION="--allow-anonymous"
 else
-  LOGIN_OPTION="--require-login"
+	LOGIN_OPTION="--require-login"
 fi
 
-if ! [ -f ./etc/broker.xml ]; then
-    CREATE_ARGUMENTS="--user ${ARTEMIS_USER} --password ${ARTEMIS_PASSWORD} --silent ${LOGIN_OPTION} ${EXTRA_ARGS}"
-    echo CREATE_ARGUMENTS=${CREATE_ARGUMENTS}
-    /opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} .
-    if [ -d ./etc-override ]; then
-        for file in `ls ./etc-override`; do echo copying file to etc folder: $file; cp ./etc-override/$file ./etc || :; done
-    fi
+if ! [ -f ./etc/broker.xml ]
+then
+	CREATE_ARGUMENTS="--user ${ARTEMIS_USER} --password ${ARTEMIS_PASSWORD} --silent ${LOGIN_OPTION} ${EXTRA_ARGS}"
+
+	echo CREATE_ARGUMENTS=${CREATE_ARGUMENTS}
+
+	/opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} .
+
+	if [ -d ./etc-override ]
+	then
+		for file in `ls ./etc-override`
+		do
+			echo copying file to etc folder: $file
+			cp ./etc-override/$file ./etc || :
+		done
+	fi
 else
-    echo "skipping broker instance creation; instance already exists"
+	echo "skipping broker instance creation; instance already exists"
 fi
 
 exec ./bin/artemis "$@"
