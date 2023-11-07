c95eb9f49bb372fcdd751c8c3bec9c87c492d3a4
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-07 14:24:08 -0800

    LRCI-3820 Add logic to configure activemq with clustering

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-sidecar-activemq/data/docker-run.sh
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-sidecar-activemq/data/docker-run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.	See the NOTICE file
+# or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
-# regarding copyright ownership.	The ASF licenses this file
+# regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
-# with the License.	You may obtain a copy of the License at
+# with the License.  You may obtain a copy of the License at
 #
-#	 http://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.	See the License for the
+# KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
 
@@ -36,11 +36,28 @@ fi
 
 if ! [ -f ./etc/broker.xml ]
 then
+
+	if [[ ${ARTEMIS_CLUSTER_PASSWORD} != "" ]] && [[ ${ARTEMIS_CLUSTER_USER} != "" ]]
+	then
+		if [[ ${ARTEMIS_CLUSTER_SLAVE} == "true" ]]
+		then
+			SLAVE_OPTION="--slave"
+		else
+			SLAVE_OPTION=""
+		fi
+
+		IP_ADDRESS=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | cut -b 6-)
+
+		CLUSTER_ARGUMENTS="--clustered --cluster-password ${ARTEMIS_CLUSTER_PASSWORD} --cluster-user ${ARTEMIS_CLUSTER_USER} --host ${IP_ADDRESS} --replicated ${SLAVE_OPTION}"
+	else
+		CLUSTER_ARGUMENTS=""
+	fi
+
 	CREATE_ARGUMENTS="--user ${ARTEMIS_USER} --password ${ARTEMIS_PASSWORD} --silent ${LOGIN_OPTION} ${EXTRA_ARGS}"
 
-	echo CREATE_ARGUMENTS=${CREATE_ARGUMENTS}
+	echo "/opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} ${CLUSTER_ARGUMENTS} ."
 
-	/opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} .
+	/opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} ${CLUSTER_ARGUMENTS} .
 
 	if [ -d ./etc-override ]
 	then
@@ -49,6 +66,16 @@ then
 			echo copying file to etc folder: $file
 			cp ./etc-override/$file ./etc || :
 		done
+	fi
+
+	if [[ ${ARTEMIS_CLUSTER_PASSWORD} != "" ]] && [[ ${ARTEMIS_CLUSTER_USER} != "" ]]
+	then
+		if [[ ${ARTEMIS_CLUSTER_SLAVE} == "true" ]]
+		then
+			sed -i -e "s|<slave/>|<slave><allow-failback>true</allow-failback></slave>|g" ./etc/broker.xml
+		else
+			sed -i -e "s|<vote-on-replication-failure|<check-for-live-server>true</check-for-live-server><vote-on-replication-failure|g" ./etc/broker.xml
+		fi
 	fi
 else
 	echo "skipping broker instance creation; instance already exists"
