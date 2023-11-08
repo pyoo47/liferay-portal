eabbf058d4d46eff483db8fc1bc96e4b68c6af9c
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-07 17:31:29 -0800

    LRCI-3820 Rename from liferay-jethr0-sidecar-activemq to liferay-jethr0-sidecar-jms-master to leave room for another image

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-sidecar-jms-master/data/docker-run.sh
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-sidecar-jms-master/data/docker-run.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-# This is the entry point for the docker images.
-# This file is executed when "docker container create" or "docker run" is called.
-
-set -e
 
 BROKER_HOME=/var/lib/
 
@@ -36,6 +15,7 @@ fi
 
 if ! [ -f ./etc/broker.xml ]
 then
+	IP_ADDRESS=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | cut -b 6-)
 
 	if [[ ${ARTEMIS_CLUSTER_PASSWORD} != "" ]] && [[ ${ARTEMIS_CLUSTER_USER} != "" ]]
 	then
@@ -45,8 +25,6 @@ then
 		else
 			SLAVE_OPTION=""
 		fi
-
-		IP_ADDRESS=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | cut -b 6-)
 
 		CLUSTER_ARGUMENTS="--clustered --cluster-password ${ARTEMIS_CLUSTER_PASSWORD} --cluster-user ${ARTEMIS_CLUSTER_USER} --host ${IP_ADDRESS} --replicated ${SLAVE_OPTION}"
 	else
@@ -76,6 +54,8 @@ then
 		else
 			sed -i -e "s|<vote-on-replication-failure|<check-for-live-server>true</check-for-live-server><vote-on-replication-failure|g" ./etc/broker.xml
 		fi
+
+		sed -i -e "s|<group-address>|<local-bind-address>${IP_ADDRESS}</local-bind-address><group-address>|g" ./etc/broker.xml
 	fi
 else
 	echo "skipping broker instance creation; instance already exists"
