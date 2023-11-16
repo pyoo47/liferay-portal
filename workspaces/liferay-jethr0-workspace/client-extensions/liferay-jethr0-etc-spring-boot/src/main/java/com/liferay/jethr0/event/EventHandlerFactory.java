bdbb1f7697836130b84ea9fe4acafa867e08f9cd
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-15 18:48:04 -0800

    LRCI-3823 Remove unnecessary REST Controller

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/EventHandlerFactory.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/EventHandlerFactory.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -27,63 +27,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class EventHandlerFactory {
-
-	public EventHandler newEventHandler(
-		EventHandler.EventType eventType, JSONObject messageJSONObject) {
-
-		EventHandler eventHandler = null;
-
-		if (eventType == EventHandler.EventType.BUILD_COMPLETED) {
-			eventHandler = new BuildCompletedEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.BUILD_STARTED) {
-			eventHandler = new BuildStartedEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if ((eventType == EventHandler.EventType.COMPUTER_BUSY) ||
-				 (eventType == EventHandler.EventType.COMPUTER_OFFLINE) ||
-				 (eventType == EventHandler.EventType.COMPUTER_ONLINE) ||
-				 (eventType ==
-					 EventHandler.EventType.COMPUTER_TEMPORARILY_OFFLINE) ||
-				 (eventType ==
-					 EventHandler.EventType.COMPUTER_TEMPORARILY_ONLINE)) {
-
-			eventHandler = new ComputerUpdateEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.COMPUTER_IDLE) {
-			eventHandler = new ComputerIdleEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.CREATE_BUILD) {
-			eventHandler = new CreateBuildEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.CREATE_BUILD_RUN) {
-			eventHandler = new CreateBuildRunEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.CREATE_JENKINS_COHORT) {
-			eventHandler = new CreateJenkinsCohortEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.CREATE_JOB) {
-			eventHandler = new CreateJobEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-		else if (eventType == EventHandler.EventType.QUEUE_JOB) {
-			eventHandler = new QueueJobEventHandler(
-				_eventHandlerContext, messageJSONObject);
-		}
-
-		if (eventHandler == null) {
-			throw new IllegalArgumentException(
-				"Invalid event type: " + eventType);
-		}
-
-		return eventHandler;
-	}
 
 	public JenkinsEventHandler newJenkinsEventHandler(
 		JenkinsEventHandler.EventType eventType, JSONObject messageJSONObject) {
