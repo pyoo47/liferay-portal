152912fb3e48856a1bb0706f55e0c368fb830fb1
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-16 11:26:15 -0800

    LRCI-3823 Add basic classes for GitHub event handling

---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/github/GitHubEventHandlerFactory.java
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jethr0.event.github;
+
+import com.liferay.jethr0.event.BaseEventHandlerFactory;
+import com.liferay.jethr0.event.EventHandler;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class GitHubEventHandlerFactory extends BaseEventHandlerFactory {
+
+	@Override
+	public EventHandler newEventHandler(JSONObject messageJSONObject)
+		throws IllegalArgumentException {
+
+		throw new IllegalArgumentException();
+	}
+
+}
