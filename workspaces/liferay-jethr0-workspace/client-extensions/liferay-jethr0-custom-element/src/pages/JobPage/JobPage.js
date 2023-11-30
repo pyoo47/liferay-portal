daa00d4ca0e16e7714d4b1c3b16d2e6fd2c54ba8
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-30 14:30:45 -0800

    LRCI-3892 Remove unnecessary JobInformation component

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-custom-element/src/pages/JobPage/JobPage.js
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-custom-element/src/pages/JobPage/JobPage.js
@@ -5,6 +5,7 @@
 
 import {Heading} from '@clayui/core';
 import ClayLayout from '@clayui/layout';
+import ClayPanel from '@clayui/panel';
 import {useState} from 'react';
 import {useParams} from 'react-router-dom';
 
@@ -14,9 +15,67 @@ import Jethr0ButtonsRow from '../../components/Jethr0ButtonsRow/Jethr0ButtonsRow
 import Jethr0Card from '../../components/Jethr0Card/Jethr0Card';
 import Jethr0NavigationBar from '../../components/Jethr0NavigationBar/Jethr0NavigationBar';
 import JobBuilds from '../../components/JobBuilds/JobBuilds';
-import JobInformation from '../../components/JobInformation/JobInformation';
+import {toLocaleString} from '../../services/DateUtil';
 import postSpringBootData from '../../services/postSpringBootData';
 import useSpringBootData from '../../services/useSpringBootData';
+
+function JobInformation({job}) {
+	if (!job) {
+		return (
+			<ClayPanel
+				collapsable
+				defaultExpanded
+				displayTitle="Job Information"
+				displayType="secondary"
+			>
+				<ClayPanel.Body>Loading...</ClayPanel.Body>
+			</ClayPanel>
+		);
+	}
+
+	return (
+		<ClayPanel
+			collapsable
+			defaultExpanded
+			displayTitle="Job Information"
+			displayType="secondary"
+		>
+			<ClayPanel.Body>
+				Job Name: {job.name}
+				<br />
+				Job ID: {job.id}
+				<br />
+				Job State: {job.state.name}
+				<br />
+				Job Type: {job.type.name}
+				<br />
+				Create Date: {toLocaleString(job.dateCreated)}
+				<br />
+				Modified Date: {toLocaleString(job.dateModified)}
+				<br />
+				Start Date: {toLocaleString(job.startDate)}
+				{job.jenkinsGitHubURL && job.jenkinsGitHubURL !== null && (
+					<>
+						<br />
+						Jenkins GitHub URL:{' '}
+						<a href={job.jenkinsGitHubURL}>
+							{job.jenkinsGitHubURL}
+						</a>
+					</>
+				)}
+				{job.portalPullRequestURL && (
+					<>
+						<br />
+						Portal Pull Request URL:{' '}
+						<a href={job.portalPullRequestURL}>
+							{job.portalPullRequestURL}
+						</a>
+					</>
+				)}
+			</ClayPanel.Body>
+		</ClayPanel>
+	);
+}
 
 function JobPage() {
 	const {id} = useParams();
