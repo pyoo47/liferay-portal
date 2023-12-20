f97b8ab2867843b566c61d7f21439cba0a4ec2cb
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-12-19 16:07:37 -0800

    LRCI-3937 Add Plugins Marketplace Apps job & build parameters

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/job/definition/PluginsMarketplaceAppsJobDefinition.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/job/definition/PluginsMarketplaceAppsJobDefinition.java
@@ -6,9 +6,15 @@
 package com.liferay.jethr0.job.definition;
 
 import com.liferay.jethr0.job.JobEntity;
+import com.liferay.jethr0.job.definition.parameter.CallbackURLJobParameterDefinition;
+import com.liferay.jethr0.job.definition.parameter.JenkinsBranchURLJobParameterDefinition;
 import com.liferay.jethr0.job.definition.parameter.JobParameterDefinition;
+import com.liferay.jethr0.job.definition.parameter.PluginsMarketplaceAppFileNameJobParameterDefinition;
+import com.liferay.jethr0.job.definition.parameter.PluginsMarketplaceAppTypeJobParameterDefinition;
+import com.liferay.jethr0.job.definition.parameter.PluginsMarketplaceAppURLJobParameterDefinition;
+import com.liferay.jethr0.job.definition.parameter.PortalReleaseVersionParameterDefinition;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -18,7 +24,21 @@ public class PluginsMarketplaceAppsJobDefinition extends BaseJobDefinition {
 
 	@Override
 	public Set<JobParameterDefinition> getJobParameterDefinitions() {
-		return Collections.emptySet();
+		Set<JobParameterDefinition> jobParameterDefinitions = new HashSet<>();
+
+		jobParameterDefinitions.add(new CallbackURLJobParameterDefinition());
+		jobParameterDefinitions.add(
+			new JenkinsBranchURLJobParameterDefinition());
+		jobParameterDefinitions.add(
+			new PluginsMarketplaceAppFileNameJobParameterDefinition());
+		jobParameterDefinitions.add(
+			new PluginsMarketplaceAppTypeJobParameterDefinition());
+		jobParameterDefinitions.add(
+			new PluginsMarketplaceAppURLJobParameterDefinition());
+		jobParameterDefinitions.add(
+			new PortalReleaseVersionParameterDefinition());
+
+		return jobParameterDefinitions;
 	}
 
 	protected PluginsMarketplaceAppsJobDefinition(JobEntity.Type type) {
