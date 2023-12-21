313ef6d070bc720ed97104e4531e73c158394666
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-12-21 11:46:41 -0800

    LRCI-3938 Add PublishPortalDockerImage job entity & definition classes

---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/job/definition/parameter/DockerImageFilterJobParameterDefinition.java
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jethr0.job.definition.parameter;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class DockerImageFilterJobParameterDefinition
+	extends BaseJobParameterDefinition {
+
+	@Override
+	public String getKey() {
+		return "dockerImageFilter";
+	}
+
+	@Override
+	public String getLabel() {
+		return "Docker Image Filter";
+	}
+
+	@Override
+	public Type getType() {
+		return Type.STRING;
+	}
+
+	@Override
+	public String getValueDefault() {
+		return "snapshot";
+	}
+
+	@Override
+	public String getValueDescription() {
+		return "Insert your Docker image filter here";
+	}
+
+	@Override
+	public String getValueRegex() {
+		return null;
+	}
+
+}
