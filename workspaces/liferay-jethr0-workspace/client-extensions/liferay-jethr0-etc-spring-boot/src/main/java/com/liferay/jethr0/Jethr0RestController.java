d9a29ab706d29b7bbe6df515d2379bdedd211d5f
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-04-17 16:31:45 -0700

    LRCI-4138 Add routine object actions & consolidate with job object actions

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/Jethr0RestController.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/Jethr0RestController.java
@@ -5,7 +5,17 @@
 
 package com.liferay.jethr0;
 
+import com.liferay.jethr0.event.EventHandler;
+import com.liferay.jethr0.event.liferay.LiferayEventHandlerFactory;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,9 +24,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Jethr0RestController {
 
+	@PostMapping("/object-actions")
+	public ResponseEntity<String> action(
+			@AuthenticationPrincipal Jwt jwt, @RequestBody String body) {
+
+		try {
+			EventHandler eventHandler =
+				_liferayEventHandlerFactory.newEventHandler(
+					new JSONObject(body));
+
+			return new ResponseEntity<>(eventHandler.process(), HttpStatus.OK);
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
 	@GetMapping("/ready")
 	public String ready() {
 		return "READY";
 	}
+
+	@Autowired
+	private LiferayEventHandlerFactory _liferayEventHandlerFactory;
 
 }
