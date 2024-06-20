d8522a0f7c6d7737bbc118fd53cf83ce374d08d6
Peter Yoo <peter.yoo@liferay.com>
2024-06-20 09:50:56 -0700

    LPD-28953 SF

--- modules/apps/object/object-storage-sugarcrm-test/src/testIntegration/java/com/liferay/object/storage/sugarcrm/internal/rest/manager/v1_0/test/SugarCRMObjectEntryManagerImplTest.java
+++ modules/apps/object/object-storage-sugarcrm-test/src/testIntegration/java/com/liferay/object/storage/sugarcrm/internal/rest/manager/v1_0/test/SugarCRMObjectEntryManagerImplTest.java
@@ -49,7 +49,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
