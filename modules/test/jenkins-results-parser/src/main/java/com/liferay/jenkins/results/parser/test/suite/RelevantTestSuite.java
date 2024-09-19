bdce1d5f53e503eafc2139a5c038af07de62abd7
Brittney Nguyen <brittney.nguyen@liferay.com>
2024-09-19 14:03:41 -0700

    LRCI-4550 Validate rules for the whole repository

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/suite/RelevantTestSuite.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/suite/RelevantTestSuite.java
@@ -12,6 +12,7 @@ import com.liferay.jenkins.results.parser.job.property.JobProperty;
 import com.liferay.jenkins.results.parser.test.batch.TestBatch;
 
 import java.io.File;
+import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,6 +33,8 @@ public class RelevantTestSuite {
 			portalAcceptancePullRequestJob.getPortalGitWorkingDirectory();
 
 		_modifiedFiles = portalGitWorkingDirectory.getModifiedFilesList();
+
+		_portalGitWorkingDirectory = portalGitWorkingDirectory;
 
 		_relevantRuleEngine = RelevantRuleEngine.getInstance(
 			portalAcceptancePullRequestJob);
@@ -60,7 +63,14 @@ public class RelevantTestSuite {
 		List<RelevantRule> relevantRules =
 			_relevantRuleEngine.getMatchingRelevantRules(_modifiedFiles);
 
-		RelevantRuleValidation.validate(relevantRules);
+		try {
+			RelevantRuleValidation.validate(
+				_portalGitWorkingDirectory.getGitRepositoryName(),
+				_portalGitWorkingDirectory.getUpstreamBranchName());
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
 
 		Collections.sort(relevantRules);
 
@@ -104,6 +114,7 @@ public class RelevantTestSuite {
 	}
 
 	private List<File> _modifiedFiles;
+	private final PortalGitWorkingDirectory _portalGitWorkingDirectory;
 	private final RelevantRuleEngine _relevantRuleEngine;
 	private final Set<JobProperty> _testBatchNamesJobProperties =
 		new HashSet<>();
