462d0e635e423bdb0a9e561005da8b916ee48bcf
Brittney Nguyen <brittney.nguyen@liferay.com>
2025-08-12 14:17:14 -0700

    LRCI-6202 Filter the previous testray case results by other users

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/TestrayCaseResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/TestrayCaseResult.java
@@ -89,17 +89,13 @@ public class TestrayCaseResult {
 		}
 
 		for (TestrayCaseResult previousTestrayCaseResult :
-				getTestrayCaseResultHistory(5)) {
+				getTestrayCaseResultHistory(10, 5, 10)) {
 
 			if (Objects.equals(getID(), previousTestrayCaseResult.getID())) {
 				continue;
 			}
 
-			if (_isSimilarError(previousTestrayCaseResult) &&
-				!Objects.equals(
-					getPullRequestSenderUsername(),
-					previousTestrayCaseResult.getPullRequestSenderUsername())) {
-
+			if (_isSimilarError(previousTestrayCaseResult)) {
 				_errorType = ErrorType.COMMON;
 
 				return _errorType;
@@ -218,7 +214,9 @@ public class TestrayCaseResult {
 		return _testrayCase;
 	}
 
-	public List<TestrayCaseResult> getTestrayCaseResultHistory(int maxCount) {
+	public List<TestrayCaseResult> getTestrayCaseResultHistory(
+		int maxCount, int maxPreviousTestrayCaseResults, int pageSize) {
+
 		List<TestrayCaseResult> testrayCaseResults = new ArrayList<>();
 
 		StringBuilder sb = new StringBuilder();
@@ -234,12 +232,29 @@ public class TestrayCaseResult {
 		try {
 			List<JSONObject> entityJSONObjects = testrayServer.requestGraphQL(
 				"caseResults", TestrayCaseResult.FIELD_NAMES, sb.toString(),
-				"dateCreated:desc", maxCount, 5);
+				"dateCreated:desc", maxCount, pageSize);
+
+			int previousTestrayCaseResultsCount = 0;
 
 			for (JSONObject entityJSONObject : entityJSONObjects) {
-				testrayCaseResults.add(
+				if (previousTestrayCaseResultsCount >=
+						maxPreviousTestrayCaseResults) {
+
+					break;
+				}
+
+				TestrayCaseResult testrayCaseResult =
 					TestrayFactory.newJSONObjectTestrayCaseResult(
-						testrayServer, entityJSONObject));
+						testrayServer, entityJSONObject);
+
+				if (!Objects.equals(
+						testrayCaseResult.getPullRequestSenderUsername(),
+						getPullRequestSenderUsername())) {
+
+					testrayCaseResults.add(testrayCaseResult);
+
+					previousTestrayCaseResultsCount++;
+				}
 			}
 		}
 		catch (IOException ioException) {
