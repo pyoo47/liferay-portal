f0299b13c82813d32e717f423992edecaa5f5aa6
Joshua Cords <joshua.cords@liferay.com>
2025-09-06 12:21:22 -0700

    LPD-64607 asset-categories-test: import should happen even if vocabulary is missing
    https://liferay.atlassian.net/browse/LPD-64607

--- modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/exportimport/data/handler/test/AssetCategoryPortletDataHandlerTest.java
+++ modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/exportimport/data/handler/test/AssetCategoryPortletDataHandlerTest.java
@@ -95,6 +95,8 @@ public class AssetCategoryPortletDataHandlerTest
 
 		_assetCategoryLocalService.deleteCategory(assetCategory);
 
+		_assetVocabularyLocalService.deleteVocabulary(assetVocabulary);
+
 		ExportImportConfiguration exportImportConfiguration =
 			_exportImportConfigurationLocalService.
 				addDraftExportImportConfiguration(
