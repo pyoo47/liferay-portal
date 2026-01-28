bfe5ee51383e758cec97a63f0013a8e5f4f34f04
Jorge Avalos <jorge.avalos@liferay.com>
2026-01-28 13:12:07 -0800

    LPD-75037 add integration test

--- modules/apps/portal-store/portal-store-test-util/src/main/java/com/liferay/portal/store/test/util/BaseStoreTestCase.java
+++ modules/apps/portal-store/portal-store-test-util/src/main/java/com/liferay/portal/store/test/util/BaseStoreTestCase.java
@@ -9,6 +9,7 @@ import com.liferay.document.library.kernel.exception.NoSuchFileException;
 import com.liferay.document.library.kernel.store.Store;
 import com.liferay.petra.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 
@@ -153,6 +154,12 @@ public abstract class BaseStoreTestCase {
 				_companyId, _repositoryId, fileName, Store.VERSION_DEFAULT));
 		Assert.assertTrue(
 			_store.hasFile(_companyId, _repositoryId, fileName, "1.1"));
+	}
+
+	@Test
+	public void testGetCompanyIds() throws Exception {
+		Assert.assertArrayEquals(
+			PortalInstancePool.getCompanyIds(), _store.getCompanyIds());
 	}
 
 	@Test
