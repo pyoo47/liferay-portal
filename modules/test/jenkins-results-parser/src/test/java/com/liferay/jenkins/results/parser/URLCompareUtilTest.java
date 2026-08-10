2e7494c300c61c1d3f0ec369e223d46aca11a1b2
CalumR23 <searagan@gmail.com>
2026-08-10 14:12:25 -0700

    LRCI-7983 Decompose the query strings with URLBuilderUtil
    Three places took a query string apart by hand. BaseBuild split on an ampersand and then on the first equals sign, URLCompareUtil ran a regular expression over the raw query, and BuildFactory pulled one parameter out with a replaceAll. All three now ask URLBuilderUtil, so no split on an ampersand or an equals sign is left in this repository.
    
    URLCompareUtil gains two fixes from the move. Its pattern required a value of at least one character, so a parameter with an empty value was dropped from the comparison entirely and two URLs that differed only there compared equal. It also compared names and values while they were still encoded, so the same value spelled with a percent twenty and with a plus sign compared unequal. Both are now decoded before comparing.
    
    BuildFactory extracted the job variant with a pattern that matched up to the next ampersand. An empty job variant did not match, and because replaceAll returns its input when nothing matches, the whole query string was adopted as the job variant. It now reads the parameter and falls through to the existing lookup when there is nothing there.

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/URLCompareUtilTest.java
@@ -0,0 +1,64 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+/**
+ * @author Calum Ragan
+ */
+public class URLCompareUtilTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testMatches() throws Exception {
+		testEquals(
+			true,
+			URLCompareUtil.matches(
+				new URL("http://test-1-1/job/x/1/"),
+				new URL("http://test-1-1/job/x/1")));
+		testEquals(
+			false,
+			URLCompareUtil.matches(
+				new URL("http://test-1-1/job/x/1/"),
+				new URL("http://test-1-1/job/y/1/")));
+	}
+
+	@Test
+	public void testMatchesWithEmptyParameterValue() throws Exception {
+		testEquals(
+			true,
+			URLCompareUtil.matches(
+				new URL("http://test-1-1/job/x?AXIS_VARIABLE=&JOB=y"),
+				new URL("http://test-1-1/job/x?AXIS_VARIABLE=&JOB=y")));
+		testEquals(
+			false,
+			URLCompareUtil.matches(
+				new URL("http://test-1-1/job/x?AXIS_VARIABLE=&JOB=y"),
+				new URL("http://test-1-1/job/x?AXIS_VARIABLE=1&JOB=y")));
+	}
+
+	@Test
+	public void testMatchesWithEquivalentParameterSpellings() throws Exception {
+		testEquals(
+			true,
+			URLCompareUtil.matches(
+				new URL("http://test-1-1/job/x?V=AWS%20%26%20CI"),
+				new URL("http://test-1-1/job/x?V=AWS+%26+CI")));
+	}
+
+	@Test
+	public void testMatchesWithReorderedParameters() throws Exception {
+		testEquals(
+			true,
+			URLCompareUtil.matches(
+				new URL("http://test-1-1/job/x?A=1&B=2"),
+				new URL("http://test-1-1/job/x?B=2&A=1")));
+	}
+
+}
